99e468f8c5b56230aca26dc07bfc5ceff8956a6a
Victor Ginelli <victorginelli@Gilbertos-MacBook-Pro-2.local>
2020-03-09 14:51:19 -0700

    Delete User mutation

--- packages/server/graphql/user/mutations/index.js
+++ packages/server/graphql/user/mutations/index.js
@@ -1,7 +1,9 @@
 const Login = require('./LoginMutation');
 const CreateUser = require('./CreateUserMutation');
+const DeleteUser = require('./DeleteUserMutation')
 
 module.exports = {
   CreateUser,
-  Login
+  Login,
+  DeleteUser
 };
