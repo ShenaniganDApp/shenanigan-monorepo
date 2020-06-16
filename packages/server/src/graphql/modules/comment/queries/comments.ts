a97166de4208de4ea65fb929a632a0ee05cdeb96
youngkidwarrior <ginelliunit@gmail.com>
2020-06-15 23:16:19 -0700

    Comments query

---
+++ packages/server/src/graphql/modules/comment/queries/comments.ts
@@ -0,0 +1,33 @@
+import { GraphQLString, GraphQLNonNull, GraphQLID } from 'graphql';
+
+import { connectionArgs, fromGlobalId } from 'graphql-relay';
+
+import CommentType, { CommentConnection } from '../CommentType';
+
+import * as CommentLoader from '../CommentLoader';
+
+export default {
+  comment: {
+    type: CommentType,
+    args: {
+      id: {
+        type: GraphQLNonNull(GraphQLID)
+      }
+    },
+    resolve: (obj, args, context) => {
+      const { id } = fromGlobalId(args.id);
+      return CommentLoader.load(context, id);
+    }
+  },
+  comments: {
+    type: CommentConnection.connectionType,
+    args: {
+      ...connectionArgs,
+      search: {
+        type: GraphQLString
+      }
+    },
+    resolve: (obj, args, context) => {   
+      return CommentLoader.loadComments(context, args)}
+  }
+};
