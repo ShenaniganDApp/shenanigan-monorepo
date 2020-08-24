0b8c9ca1eabedd2433e7b281166307263d1a316e
youngkidwarrior <ginelliunit@gmail.com>
2020-08-24 04:34:58 -0700

    setup migrations

--- shenanigan-monorepo/packages/contracts/migrations/2_deploy_contracts.js
+++ shenanigan-monorepo/packages/contracts/migrations/2_deploy_contracts.js
@@ -1,16 +1,4 @@
-const IShenanigan = artifacts.require('ISHenanigan');
-const Shenanigan = artifacts.require('Shenanigan');
-const ShenaniganDataTypes = artifacts.require('ShenaniganDataTypes');
-const ShenaniganStorage = artifacts.require('ShenaniganStorage');
-const Election = artifacts.require('Election');
-const Wager = artifacts.require('Wager');
-const Voting = artifacts.require('Voting');
+const ChallengeFactory = artifacts.require('ChallengeFactory');
 module.exports = function (deployer) {
-  deployer.deploy(IShenanigan);
-  deployer.deploy(Shenanigan);
-  deployer.deploy(ShenaniganDataTypes);
-  deployer.deploy(ShenaniganStorage);
-  deployer.deploy(Election);
-  deployer.deploy(Wager);
-  deployer.deploy(Voting);
+  deployer.deploy(ChallengeFactory);
 };
