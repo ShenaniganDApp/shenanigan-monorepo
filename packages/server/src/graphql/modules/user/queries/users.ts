1e14f40f25aab4bc3e775412b251d500454fc135
youngkidwarrior <ginelliunit@gmail.com>
2020-08-31 18:06:43 -0700

    fix users query

--- packages/server/src/graphql/modules/user/queries/users.ts
+++ packages/server/src/graphql/modules/user/queries/users.ts
@@ -8,32 +8,32 @@ import UserType, { UserConnection } from '../UserType';
 import * as UserLoader from '../UserLoader';
 
 export default {
-  me: {
-    type: UserType,
-    resolve: (root, args, context) => {
-      return context.user ? UserLoader.load(context, context.user._id) : null;
-    }
-  },
-  user: {
-    type: UserType,
-    args: {
-      _id: {
-        type: new GraphQLNonNull(GraphQLString)
-      }
-    },
-    resolve: (obj, args, context) => {
-      // const { id } = fromGlobalId(args.id);
-      return UserLoader.load(context, args._id);
-    }
-  },
-  users: {
-    type: UserConnection.connectionType,
-    args: {
-      ...connectionArgs,
-      search: {
-        type: GraphQLString
-      }
-    },
-    resolve: (obj, args, context) => UserLoader.loadUsers(context, args)
-  }
+	me: {
+		type: UserType,
+		resolve: (root, args, context) => {
+			return context.user ? UserLoader.load(context, context.user._id) : null;
+		},
+	},
+	user: {
+		type: UserType,
+		args: {
+			id: {
+				type: GraphQLNonNull(GraphQLID),
+			},
+		},
+		resolve: (obj, args, context) => {
+			const { id } = fromGlobalId(args.id);
+			return UserLoader.load(context, id);
+		},
+	},
+	users: {
+		type: UserConnection.connectionType,
+		args: {
+			...connectionArgs,
+			search: {
+				type: GraphQLString,
+			},
+		},
+		resolve: (obj, args, context) => UserLoader.loadUsers(context, args),
+	},
 };
