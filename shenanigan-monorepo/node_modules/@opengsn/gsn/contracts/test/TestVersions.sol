bc882307e09868c604e06ac596c4c6c820f3b4ee
youngkidwarrior <ginelliunit@gmail.com>
2020-09-03 22:55:30 -0700

    fix file structure

--- shenanigan-monorepo/node_modules/@opengsn/gsn/contracts/test/TestVersions.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier:MIT
-pragma solidity ^0.6.2;
-
-contract TestVersions {
-
-    function versionHub() external view returns (string memory) {
-        return "3.0.0";
-    }
-}
--- shenanigan-monorepo/node_modules/@opengsn/gsn/dist/src/common/ErrorReplacerJSON.d.ts
+++
@@ -1,3 +0,0 @@
-export default function replaceErrors(key: string, value: {
-    [key: string]: any;
-}): any;
