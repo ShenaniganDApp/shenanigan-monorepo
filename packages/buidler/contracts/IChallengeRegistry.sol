105d6a17bf104bfd876847554ef43aa8e4b7e763
youngkidwarrior <ginelliunit@gmail.com>
2020-09-12 19:04:54 -0700

    Contract registry for AMB

---
+++ packages/buidler/contracts/IChallengeRegistry.sol
@@ -0,0 +1,21 @@
+/*
+  This file is part of Shenanigan.
+  Shenanigan is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  Shenanigan is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with Shenanigan. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+pragma solidity ^0.6.8;
+
+interface IChallengeRegistry {
+    function challengeAddress() external view returns (address);
+    function bridgeMediatorAddress() external view returns (address);
+    function trustedForwarder() external view returns (address);
+}
