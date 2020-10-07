2ac9c25405cd41b64db586454ecc71ddddb451a6
YoungKidWarrior <ginelliunit@gmail.com>
2020-10-06 23:30:10 -0700

    Add navigator & routes for Profile

--- packages/app/src/components/profile/Profile.tsx
+++ packages/app/src/components/profile/Profile.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useQuery, graphql, useFragment } from 'relay-hooks';
-import { ProfileTabsProps } from '../../Navigator';
-import { Profile_me$key} from './__generated__/Profile_me.graphql';
+import { ProfileProps } from '../../Navigator';
+import { Profile_me$key } from './__generated__/Profile_me.graphql';
 
 const fragmentSpecMe = graphql`
-  fragment Profile_me on User {
-    id
-    username
-  }
+    fragment Profile_me on User {
+        id
+        username
+    }
 `;
 
-type Props = ProfileTabsProps;
+type Props = ProfileProps;
 const Profile = (props: Props) => {
-    const me = useFragment<Profile_me$key>(fragmentSpecMe, props.route.params.me);
+    const me = useFragment<Profile_me$key>(
+        fragmentSpecMe,
+        props.route.params.me
+    );
     return (
         <SafeAreaView>
             <Text> {me?.id} </Text>
             <Text> {me?.username} </Text>
+            <Button
+                title="Start Streaming"
+                onPress={() => props.navigation.navigate('LiveDashboard')}
+            ></Button>
         </SafeAreaView>
     );
 };
