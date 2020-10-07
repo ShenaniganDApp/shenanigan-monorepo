1fec1a2b7d94eeca1c4c9526292cc0233ee67769
YoungKidWarrior <ginelliunit@gmail.com>
2020-10-06 23:10:01 -0700

    convert Live to functional component

--- packages/app/src/components/Live/Live.tsx
+++ packages/app/src/components/Live/Live.tsx
@@ -1,24 +1,20 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Component } from 'react';
-import { View, Text, Linking, Alert, Button } from 'react-native';
-import { MainTabsProps as Props } from '../../Navigator';
+import { View, Text, Linking, Alert, Button, PermissionsAndroid } from 'react-native';
+import { LiveTabsProps as Props } from '../../Navigator';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+type State = {
+  
+};
 
-export default class Live extends Component<Props> {
-    constructor(props: Props) {
-        super(props);
-        console.log('props: ', props);
-    }
-    state = {};
-
-    render() {
-        const { address } = this.props.route.params;
-        return (
-            <SafeAreaView style={{ flex: 1, backgroundColor: '#d2ffff' }}>
-                <Text>Live</Text>
-                <Text>accounts: {address}</Text>
-            </SafeAreaView>
-        );
-    }
+export default function Live(props: Props) {
+    const { address } = props.route.params;
+   
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#d2ffff' }}>
+            <Text>Live</Text>
+            <Text>accounts: {address}</Text>
+        </SafeAreaView>
+    );
 }
