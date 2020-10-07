f11b3163838ed0ade1a2c25619002d08f8194b30
YoungKidWarrior <ginelliunit@gmail.com>
2020-10-06 23:28:23 -0700

    Live Dashboard compoent
    This component wll house the options available to an iOn prior to going
    live or when they are live

---
+++ packages/app/src/components/LiveDashboard/LiveDashboard.tsx
@@ -0,0 +1,72 @@
+import React, { useState, useRef } from 'react';
+import {
+    View,
+    Text,
+    Linking,
+    Alert,
+    Button,
+    PermissionsAndroid
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NodeCameraView } from 'react-native-nodemediaclient';
+
+type Props = {};
+
+export default function LiveDashboard(props: Props) {
+    const [isPublish, setIsPublish] = useState(false);
+    const [publishBtnTitle, setPublishBtnTitle] = useState('Start Publish');
+
+    const vb = useRef<any>(null);
+
+    async function requestCameraPermission() {
+        try {
+            const granted = await PermissionsAndroid.requestMultiple([
+                PermissionsAndroid.PERMISSIONS.CAMERA,
+                PermissionsAndroid.PERMISSIONS.RECORD_AUDIO
+            ]);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#d2ffff' }}>
+            <NodeCameraView
+                style={{ height: 400 }}
+                ref={vb}
+                outputUrl={'/'}
+                camera={{ cameraId: 1, cameraFrontMirror: true }}
+                audio={{ bitrate: 32000, profile: 1, samplerate: 44100 }}
+                video={{
+                    preset: 12,
+                    bitrate: 400000,
+                    profile: 1,
+                    fps: 60,
+                    videoFrontMirror: false
+                }}
+                autopreview={true}
+            />
+
+            <Button
+                title="request permissions"
+                onPress={requestCameraPermission}
+            />
+            <Button
+                onPress={() => {
+                    if (isPublish) {
+                        setPublishBtnTitle('Start Publish');
+                        setIsPublish(false);
+
+                        vb.current.stop();
+                    } else {
+                        setPublishBtnTitle('Stop Publish');
+                        setIsPublish(true);
+
+                        vb.current.start();
+                    }
+                }}
+                title={publishBtnTitle}
+                color="#841584"
+            />
+        </SafeAreaView>
+    );
+}
