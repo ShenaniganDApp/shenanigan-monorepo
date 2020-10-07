d6b5ffac0164250c9845aad03490ba9f6ec7fd57
YoungKidWarrior <ginelliunit@gmail.com>
2020-10-06 23:30:10 -0700

    Add navigator & routes for Profile

--- packages/app/src/Navigator.tsx
+++ packages/app/src/Navigator.tsx
@@ -5,23 +5,60 @@ import {
     MaterialTopTabScreenProps
 } from '@react-navigation/material-top-tabs';
 
+import {
+    createStackNavigator,
+    StackScreenProps
+} from '@react-navigation/stack';
+
+import { CompositeNavigationProp } from '@react-navigation/native';
+
 import Profile from './components/profile/Profile';
 import Live from './components/Live/Live';
 import Poll from './components/Poll/Poll';
+import LiveDashboard from './components/LiveDashboard/LiveDashboard';
 
 import { AppQueryResponse } from './__generated__/AppQuery.graphql';
 
 export type MainTabsParams = {
     Live: { address: string };
-    Profile: { address: string } & AppQueryResponse;
+    ProfileStack: { address: string } & AppQueryResponse;
     Poll: {};
 };
 
 export type LiveTabsProps = MaterialTopTabScreenProps<MainTabsParams, 'Live'>;
 export type ProfileTabsProps = MaterialTopTabScreenProps<
     MainTabsParams,
+    'ProfileStack'
+>;
+
+export type ProfileStackParams = {
+    Profile: {} & AppQueryResponse;
+    LiveDashboard: {};
+};
+
+export type ProfileProps = StackScreenProps<
+    ProfileStackParams,
     'Profile'
 >;
+
+export type LiveDashboardProps = StackScreenProps<
+    ProfileStackParams,
+    'LiveDashboard'
+>;
+
+const ProfileStackNavigator = createStackNavigator<ProfileStackParams>();
+
+function ProfileStack({ address, me }: { address: string } & AppQueryResponse) {
+    return (
+        <ProfileStackNavigator.Navigator initialRouteName={'Profile'}>
+            <ProfileStackNavigator.Screen name="Profile" component={Profile} initialParams={{me}} />
+            <ProfileStackNavigator.Screen
+                name="LiveDashboard"
+                component={LiveDashboard}
+            />
+        </ProfileStackNavigator.Navigator>
+    );
+}
 
 const MainTabNavigator = createMaterialTopTabNavigator<MainTabsParams>();
 export function MainTabsStack({
@@ -30,7 +67,7 @@ export function MainTabsStack({
     retry
 }: {
     address: string;
-    retry : unknown
+    retry: unknown;
 } & AppQueryResponse) {
     return (
         <MainTabNavigator.Navigator
@@ -38,8 +75,8 @@ export function MainTabsStack({
             tabBarOptions={{ style: { display: 'none' } }}
         >
             <MainTabNavigator.Screen
-                name="Profile"
-                component={Profile}
+                name="ProfileStack"
+                component={ProfileStack}
                 initialParams={{ address, me }}
             />
             <MainTabNavigator.Screen
