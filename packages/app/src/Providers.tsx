9641d30a1b165a9bd4d1f61e9afe72e62fdb48ee
YoungKidWarrior <ginelliunit@gmail.com>
2020-10-08 17:14:47 -0700

    Use webview with react native

--- packages/app/src/Providers.tsx
+++ packages/app/src/Providers.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { RelayEnvironmentProvider } from 'relay-hooks';
+import WalletConnectProvider from 'react-native-walletconnect';
+
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import App from "./App"
+import App from './App';
 import Environment from './relay/Environment';
 
 const Providers = () => {
     return (
         <RelayEnvironmentProvider environment={Environment}>
-            <SafeAreaProvider><App/></SafeAreaProvider>
+            <WalletConnectProvider>
+                <SafeAreaProvider>
+                    <App />
+                </SafeAreaProvider>
+            </WalletConnectProvider>
         </RelayEnvironmentProvider>
     );
 };
--- packages/app/src/connectWallet.ts
+++ packages/app/src/connectWallet.ts
@@ -1,74 +1,74 @@
-import WalletConnect from '@walletconnect/client';
-import { IConnector } from '@walletconnect/types';
+// import WalletConnect from '@walletconnect/client';
+// import { IConnector } from '@walletconnect/types';
 
-let connector: IConnector;
+// let connector: IConnector;
 
-// const readContracts = useContractLoader(localProvider);
+// // const readContracts = useContractLoader(localProvider);
 
-/**
- * Create a new WalletConnect connector
- *
- * @param connector
- * @param opts
- */
-function createConnector(opts) {
-    const connector = new WalletConnect(opts);
-    return connector;
-}
-// Check if connection is already established
+// /**
+//  * Create a new WalletConnect connector
+//  *
+//  * @param connector
+//  * @param opts
+//  */
+// function createConnector(opts) {
+//     const connector = new WalletConnect(opts);
+//     return connector;
+// }
+// // Check if connection is already established
 
-function onDisplayURI(err, payload) {
-    if (err) {
-        throw err;
-    }
-    return payload.params[0];
+// function onDisplayURI(err, payload) {
+//     if (err) {
+//         throw err;
+//     }
+//     return payload.params[0];
 
-    throw new Error('URI missing from display_uri');
-}
+//     throw new Error('URI missing from display_uri');
+// }
 
-function onConnect(err, payload) {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected', payload);
-    sendPing();
-}
+// function onConnect(err, payload) {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log('Connected', payload);
+//     sendPing();
+// }
 
-function onSessionUpdate(err, payload) {
-    if (err) {
-        throw err;
-    }
-    console.log('Session updated', payload);
-}
+// function onSessionUpdate(err, payload) {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log('Session updated', payload);
+// }
 
-function onPing(err, payload) {
-    if (err) {
-        throw err;
-    }
-    console.log('Ping received', payload);
-}
+// function onPing(err, payload) {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log('Ping received', payload);
+// }
 
-function sendPing() {
-    connector.sendCustomRequest({ method: 'ping' });
-}
+// function sendPing() {
+//     connector.sendCustomRequest({ method: 'ping' });
+// }
 
-function setupConnection(){
-    let uri = ""
-    connector = createConnector({
-        bridge: 'https://bridge.walletconnect.org'
-    });
-    connector.on('display_uri', (err, payload) => {
-        uri = onDisplayURI(err, payload);
-    });
-    connector.on('connect', (err, payload) => {
-        onConnect(err, payload);
-    });
-    connector.on('session_update', (err, payload) => {
-        onSessionUpdate(err, payload);
-    });
-    console.log('Creating session');
-    connector.createSession();
-    return uri;
-}
+// function setupConnection(){
+//     let uri = ""
+//     connector = createConnector({
+//         bridge: 'https://bridge.walletconnect.org'
+//     });
+//     connector.on('display_uri', (err, payload) => {
+//         uri = onDisplayURI(err, payload);
+//     });
+//     connector.on('connect', (err, payload) => {
+//         onConnect(err, payload);
+//     });
+//     connector.on('session_update', (err, payload) => {
+//         onSessionUpdate(err, payload);
+//     });
+//     console.log('Creating session');
+//     connector.createSession();
+//     return uri;
+// }
 
-export default setupConnection
+// export default setupConnection
