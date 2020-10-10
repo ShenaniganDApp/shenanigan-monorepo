c1f458e41d1421ebf67a78af4b0ddcd37a090176
YoungKidWarrior <ginelliunit@gmail.com>
2020-10-09 18:27:25 -0700

    live player view

--- packages/app/src/components/Live/Live.tsx
+++ packages/app/src/components/Live/Live.tsx
@@ -1,20 +1,35 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Component } from 'react';
-import { View, Text, Linking, Alert, Button, PermissionsAndroid } from 'react-native';
+import {
+    View,
+    Text,
+    Linking,
+    Alert,
+    Button,
+    PermissionsAndroid
+} from 'react-native';
 import { LiveTabsProps as Props } from '../../Navigator';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-type State = {
-  
-};
+import { NodePlayerView } from 'react-native-nodemediaclient';
 
 export default function Live(props: Props) {
     const { address } = props.route.params;
-   
+    const vp = useRef(null);
+
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: '#d2ffff' }}>
-            <Text>Live</Text>
-            <Text>accounts: {address}</Text>
+        <NodePlayerView
+          style={{ flex: 1, backgroundColor: '#333' }}
+          ref={vp}
+          inputUrl={""}
+          scaleMode={"ScaleAspectFill"}
+          bufferTime={300}
+          maxBufferTime={1000}
+          autoplay={true}
+          onStatus={(code, msg) => {
+            console.log("onStatus=" + code + " msg=" + msg);
+          }}
+        />
         </SafeAreaView>
     );
 }
