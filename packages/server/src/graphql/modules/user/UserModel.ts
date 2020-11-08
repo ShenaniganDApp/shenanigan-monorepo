dc8ac7e2543046a15266d1a24b75629a0dab107a
YoungKidWarrior <ginelliunit@gmail.com>
2020-11-07 19:22:12 -0800

    make the address and DID required

--- packages/server/src/graphql/modules/user/UserModel.ts
+++ packages/server/src/graphql/modules/user/UserModel.ts
@@ -1,76 +1,76 @@
-import bcrypt from "bcryptjs";
-import mongoose, { Document, Model, Types } from "mongoose";
+import bcrypt from 'bcryptjs';
+import mongoose, { Document, Model, Types } from 'mongoose';
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-    },
-    addresses: [
-      {
-        type: String,
-        required: false,
-        unique: true,
-      },
-    ],
-    DID: {
-      type: String,
-      required: false,
-      unique: true,
-    },
-    createdChallenges: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Challenge",
-      },
-    ],
-    predictions: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Prediction",
-      },
-    ],
-    donations: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Donation",
-      },
-    ],
-  },
-  { timestamps: true, collection: "users" }
+	{
+		username: {
+			type: String,
+		},
+		addresses: [
+			{
+				type: String,
+				required: false,
+				unique: true,
+			},
+		],
+		DID: {
+			type: String,
+			required: false,
+			unique: true,
+		},
+		createdChallenges: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Challenge',
+			},
+		],
+		predictions: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Prediction',
+			},
+		],
+		donations: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Donation',
+			},
+		],
+	},
+	{ timestamps: true, collection: 'users' }
 );
 
 export interface IUser extends Document {
-  username: string;
-  addresses: string[];
-  DID: string;
-  createdChallenges: Types.ObjectId[];
-  predictions: Types.ObjectId[];
-  donations: Types.ObjectId[];
-  authenticate: (plainTextDID: string) => boolean;
-  encryptDID: (address: string | undefined) => string;
+	username: string;
+	addresses: string[];
+	DID: string;
+	createdChallenges: Types.ObjectId[];
+	predictions: Types.ObjectId[];
+	donations: Types.ObjectId[];
+	authenticate: (plainTextDID: string) => boolean;
+	encryptDID: (address: string | undefined) => string;
 }
 
-userSchema.pre<IUser>("save", function encryptDIDHook(next) {
-  // Hash the password
-  if (this.isModified("DID")) {
-    this.DID = this.encryptDID(this.DID);
-  }
+userSchema.pre<IUser>('save', function encryptDIDHook(next) {
+	// Hash the password
+	if (this.isModified('DID')) {
+		this.DID = this.encryptDID(this.DID);
+	}
 
-  return next();
+	return next();
 });
 
 userSchema.methods = {
-  authenticate(plainTextDID: string) {
-    return bcrypt.compareSync(plainTextDID, this.DID);
-  },
-  encryptDID(DID: string) {
-    return bcrypt.hashSync(DID, 8);
-  },
+	authenticate(plainTextDID: string) {
+		return bcrypt.compareSync(plainTextDID, this.DID);
+	},
+	encryptDID(DID: string) {
+		return bcrypt.hashSync(DID, 8);
+	},
 };
 
-const UserModel: Model<IUser> = mongoose.model("User", userSchema);
+const UserModel: Model<IUser> = mongoose.model('User', userSchema);
 
 export default UserModel;
--- packages/server/src/graphql/modules/user/UserType.ts
+++ packages/server/src/graphql/modules/user/UserType.ts
@@ -1,96 +1,87 @@
-import {
-  GraphQLID,
-  GraphQLList,
-  GraphQLNonNull,
-  GraphQLObjectType,
-  GraphQLString,
-} from "graphql";
-import { globalIdField } from "graphql-relay";
+import { GraphQLID, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
+import { globalIdField } from 'graphql-relay';
 
-import {
-  ChallengeLoader,
-  DonationLoader,
-  PredictionLoader,
-} from "../../loaders";
-import { GraphQLContext } from "../../TypeDefinition";
-import { connectionArgs, connectionDefinitions, withFilter } from "../../utils";
-import { ChallengeConnection } from "../challenge/ChallengeType";
-import { DonationConnection } from "../donation/DonationType";
-import { nodeInterface, registerTypeLoader } from "../node/typeRegister";
-import { PredictionConnection } from "../prediction/PredictionType";
-import { load } from "./UserLoader";
-import { IUser } from "./UserModel";
+import { ChallengeLoader, DonationLoader, PredictionLoader } from '../../loaders';
+import { GraphQLContext } from '../../TypeDefinition';
+import { connectionArgs, connectionDefinitions, withFilter } from '../../utils';
+import { ChallengeConnection } from '../challenge/ChallengeType';
+import { DonationConnection } from '../donation/DonationType';
+import { nodeInterface, registerTypeLoader } from '../node/typeRegister';
+import { PredictionConnection } from '../prediction/PredictionType';
+import { load } from './UserLoader';
+import { IUser } from './UserModel';
 
 const UserType = new GraphQLObjectType<IUser, GraphQLContext>({
-  name: "User",
-  description: "User data",
-  fields: () => ({
-    id: globalIdField("User"),
-    _id: {
-      type: GraphQLNonNull(GraphQLID),
-      resolve: (user) => user._id,
-    },
-    username: {
-      type: GraphQLString,
-      resolve: (user) => user.username,
-    },
-    addresses: {
-      type: GraphQLNonNull(GraphQLList(GraphQLString)),
-      resolve: (user) => user.addresses,
-    },
-    DID: {
-      type: GraphQLNonNull(GraphQLList(GraphQLString)),
-      resolve: (user) => user.addresses,
-    },
-    predictions: {
-      type: GraphQLNonNull(PredictionConnection.connectionType),
-      args: {
-        ...connectionArgs,
-      },
-      resolve: async (user, args, context) =>
-        await PredictionLoader.loadAll(
-          context,
-          withFilter(args, {
-            creator: user._id,
-          })
-        ),
-    },
-    donations: {
-      type: GraphQLNonNull(DonationConnection.connectionType),
-      args: {
-        ...connectionArgs,
-      },
-      resolve: async (user, args, context) =>
-        await DonationLoader.loadAll(
-          context,
-          withFilter(args, {
-            creator: user._id,
-          })
-        ),
-    },
-    createdChallenges: {
-      type: GraphQLNonNull(ChallengeConnection.connectionType),
-      args: {
-        ...connectionArgs,
-      },
-      resolve: async (user, args, context) =>
-        await ChallengeLoader.loadAll(
-          context,
-          withFilter(
-            { args },
-            {
-              creator: user._id,
-            }
-          )
-        ),
-    },
-  }),
-  interfaces: () => [nodeInterface],
+	name: 'User',
+	description: 'User data',
+	fields: () => ({
+		id: globalIdField('User'),
+		_id: {
+			type: GraphQLNonNull(GraphQLID),
+			resolve: (user) => user._id,
+		},
+		username: {
+			type: GraphQLString,
+			resolve: (user) => user.username,
+		},
+		addresses: {
+			type: GraphQLNonNull(GraphQLList(GraphQLString)),
+			resolve: (user) => user.addresses,
+		},
+		DID: {
+			type: GraphQLNonNull(GraphQLString),
+			resolve: (user) => user.DID,
+		},
+		predictions: {
+			type: GraphQLNonNull(PredictionConnection.connectionType),
+			args: {
+				...connectionArgs,
+			},
+			resolve: async (user, args, context) => {
+				await PredictionLoader.loadAll(
+					context,
+					withFilter(args, {
+						creator: user._id,
+					})
+				);
+			},
+		},
+		donations: {
+			type: GraphQLNonNull(DonationConnection.connectionType),
+			args: {
+				...connectionArgs,
+			},
+			resolve: async (user, args, context) =>
+				await DonationLoader.loadAll(
+					context,
+					withFilter(args, {
+						creator: user._id,
+					})
+				),
+		},
+		createdChallenges: {
+			type: GraphQLNonNull(ChallengeConnection.connectionType),
+			args: {
+				...connectionArgs,
+			},
+			resolve: async (user, args, context) =>
+				await ChallengeLoader.loadAll(
+					context,
+					withFilter(
+						{ args },
+						{
+							creator: user._id,
+						}
+					)
+				),
+		},
+	}),
+	interfaces: () => [nodeInterface],
 });
 
 export const UserConnection = connectionDefinitions({
-  name: "User",
-  nodeType: UserType,
+	name: 'User',
+	nodeType: UserType,
 });
 
 registerTypeLoader(UserType, load);
