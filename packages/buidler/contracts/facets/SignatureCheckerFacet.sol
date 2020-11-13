641f33b8ef00c289ee1fadaf474f92ce99036008
Ed Zynda <ezynda3@gmail.com>
2020-11-13 15:23:46 +0800

    create signature checker facet

---
+++ packages/buidler/contracts/facets/SignatureCheckerFacet.sol
@@ -0,0 +1,47 @@
+/*
+  This file is part of Shenanigan.
+  Shenanigan is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  Shenanigan is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with Shenanigan. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+pragma solidity >=0.6.0 <0.7.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "../IERC1271.sol";
+
+contract SignatureCheckerFacet is Ownable {
+    using ECDSA for bytes32;
+    using Address for address;
+    bool public checkSignatureFlag;
+
+    bytes4 internal constant _INTERFACE_ID_ERC1271 = 0x1626ba7e;
+    bytes4 internal constant _ERC1271FAILVALUE = 0xffffffff;
+
+    function setCheckSignatureFlag(bool newFlag) public onlyOwner {
+      checkSignatureFlag = newFlag;
+    }
+
+    function getSigner(bytes32 signedHash, bytes memory signature) public pure returns (address)
+    {
+        return signedHash.toEthSignedMessageHash().recover(signature);
+    }
+
+    function checkSignature(bytes32 signedHash, bytes memory signature, address checkAddress) public view returns (bool) {
+      if(checkAddress.isContract()) {
+        return IERC1271(checkAddress).isValidSignature(signedHash, signature) == _INTERFACE_ID_ERC1271;
+      } else {
+        return getSigner(signedHash, signature) == checkAddress;
+      }
+    }
+
+}
