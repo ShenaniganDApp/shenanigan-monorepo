6a2cea8f3b07ed7f0cc47b85de048f528e4ea770
Youssef KH <ucefkh@Youssefs-MacBook-Pro.local>
2020-11-19 09:43:24 +0100

    3box get address profile data

--- packages/app/src/components/profile/Profile.tsx
+++ packages/app/src/components/profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { graphql, useFragment, useQuery } from 'relay-hooks';
@@ -15,14 +15,31 @@ const fragmentSpecMe = graphql`
 
 type Props = ProfileProps;
 const Profile = (props: Props) => {
+    const [profile, setProfile] = useState('')
     const me = useFragment<Profile_me$key>(
         fragmentSpecMe,
         props.route.params.me
     );
+    const api3Box = 'https://ipfs.3box.io/profile?address='
+
+    useEffect(()=>{
+        console.log('props', props);
+        
+        const address = props.route.params.address ? props.route.params.address : ''
+        fetchProfile(address)
+    },[profile])
+
+    const fetchProfile = (address)=>{
+        fetch( api3Box + address)
+        .then(response => response.json())
+        .then(data => setProfile(data));
+    }
+
     return (
         <SafeAreaView>
             <Text> {me?.id} </Text>
             <Text> {me?.username} </Text>
+            <Text> {props.route.params.address} </Text>
             <Button
                 title="Start Streaming"
                 onPress={() => props.navigation.navigate('LiveDashboard')}
