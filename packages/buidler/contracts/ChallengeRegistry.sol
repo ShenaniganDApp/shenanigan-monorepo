48a0b8a974345a9aa71ea512306c8536bd4317f0
Ed Zynda <ezynda3@gmail.com>
2020-11-23 15:34:11 +0800

    Mint challenge NFT once a challenge is resolved

--- packages/buidler/contracts/ChallengeRegistry.sol
+++ packages/buidler/contracts/ChallengeRegistry.sol
@@ -17,11 +17,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ChallengeRegistry is Ownable {
     address public challengeAddress;
+    address public challengeTokenAddress;
     address public bridgeMediatorAddress;
     address public trustedForwarder;
 
     function setChallengeAddress(address _address) public onlyOwner {
         challengeAddress = _address;
+    }
+
+    function setChallengeTokenAddress(address _address) public onlyOwner {
+        challengeTokenAddress = _address;
     }
 
     function setBridgeMediatorAddress(address _address) public onlyOwner {
--- packages/buidler/contracts/IChallengeRegistry.sol
+++ packages/buidler/contracts/IChallengeRegistry.sol
@@ -16,6 +16,7 @@ pragma solidity ^0.6.8;
 
 interface IChallengeRegistry {
     function challengeAddress() external view returns (address);
+    function challengeTokenAddress() external view returns (address);
     function bridgeMediatorAddress() external view returns (address);
     function trustedForwarder() external view returns (address);
 }
