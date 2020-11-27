043491aa1dc074cd1593f286a247db183c75674b
YoungKidWarrior <ginelliunit@gmail.com>
2020-11-26 22:55:01 -0800

    Fix UserContexttype so it never gives undefined

--- packages/server/src/graphql/getContext.ts
+++ packages/server/src/graphql/getContext.ts
@@ -1,20 +1,20 @@
-import { getDataloaders } from "./loaders/loaderRegister";
-import { IUser } from "./modules/user/UserModel";
-import { GraphQLContext } from "./TypeDefinition";
+import { getDataloaders } from './loaders/loaderRegister';
+import { IUser } from './modules/user/UserModel';
+import { GraphQLContext } from './TypeDefinition';
 
 type ContextVars = {
-  user?: IUser | null;
+	user: IUser | null;
 };
-export const getContext = async (ctx: ContextVars = {}) => {
-  const context: GraphQLContext = {
-    ...ctx,
-  };
+export const getContext = async (ctx: ContextVars = { user: null }) => {
+	const context: GraphQLContext = {
+		...ctx,
+	};
 
-  const dataloaders = getDataloaders();
+	const dataloaders = getDataloaders();
 
-  return {
-    req: {},
-    dataloaders,
-    ...context,
-  };
+	return {
+		req: {},
+		dataloaders,
+		...context,
+	};
 };
