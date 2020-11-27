cd4c10e69f18ab40e7539f6695ad2b02b86ade94
YoungKidWarrior <ginelliunit@gmail.com>
2020-11-26 23:00:47 -0800

    Make Create and Delete Challenge Card Mutations

--- packages/server/src/graphql/schema/type/MutationType.ts
+++ packages/server/src/graphql/schema/type/MutationType.ts
@@ -1,18 +1,20 @@
-import { GraphQLObjectType } from "graphql";
+import { GraphQLObjectType } from 'graphql';
 
-import ChallengeMutations from "../../modules/challenge/mutations";
-import CommentMutations from "../../modules/comment/mutations";
-import DonationMutations from "../../modules/donation/mutations";
-import PredictionMutations from "../../modules/prediction/mutations";
-import * as UserMutations from "../../modules/user/mutations";
+import ChallengeMutations from '../../modules/challenge/mutations';
+import CommentMutations from '../../modules/comment/mutations';
+import DonationMutations from '../../modules/donation/mutations';
+import PredictionMutations from '../../modules/prediction/mutations';
+import * as UserMutations from '../../modules/user/mutations';
+import * as ChallengeCardMutations from '../../modules/challengecard/mutations';
 
 export const MutationType = new GraphQLObjectType({
-  name: "Mutation",
-  fields: () => ({
-    ...UserMutations,
-    ...ChallengeMutations,
-    ...PredictionMutations,
-    ...CommentMutations,
-    ...DonationMutations,
-  }),
+	name: 'Mutation',
+	fields: () => ({
+		...UserMutations,
+		...ChallengeMutations,
+		...PredictionMutations,
+		...CommentMutations,
+		...DonationMutations,
+		...ChallengeCardMutations,
+	}),
 });
