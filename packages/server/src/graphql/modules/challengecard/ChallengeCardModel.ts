37ef118c5060df56e6e08d7de896ea845b2e0892
YoungKidWarrior <ginelliunit@gmail.com>
2020-11-26 22:59:49 -0800

    Add ChallengeCard Module
    Challenge Cards will be minted each time a challenge is resolved

---
+++ packages/server/src/graphql/modules/challengecard/ChallengeCardModel.ts
@@ -0,0 +1,64 @@
+import mongoose, { Document, Model, Types } from 'mongoose';
+
+const { Schema } = mongoose;
+
+const challengeCardSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		content: {
+			type: String,
+		},
+		ipfs: {
+			type: String,
+			required: true,
+		},
+		streamUrl: {
+			type: String,
+			required: true,
+		},
+		result: {
+			type: Number,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		totalMint: {
+			type: Number,
+			required: true,
+		},
+		creator: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		challenge: {
+			type: Schema.Types.ObjectId,
+			ref: 'Challenge',
+			required: true,
+		},
+	},
+	{ timestamps: true, collection: 'challengecards' }
+);
+
+export interface IChallengeCard extends Document {
+	title: string;
+	content?: string;
+	ipfs: string;
+	streamUrl: string;
+	result: number;
+	price: number;
+	totalMint: number;
+	creator: Types.ObjectId;
+	challenge: Types.ObjectId;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+const ChallengeCardModel: Model<IChallengeCard> = mongoose.model('ChallengeCard', challengeCardSchema);
+
+export default ChallengeCardModel;
