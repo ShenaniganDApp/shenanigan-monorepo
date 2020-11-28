a100f3c937e16ef2428fcb6ecc4c3dc896ac07b4
YoungKidWarrior <ginelliunit@gmail.com>
2020-11-27 22:49:20 -0800

    run format

--- packages/server/src/graphql/modules/challenge/ChallengeType.ts
+++ packages/server/src/graphql/modules/challenge/ChallengeType.ts
@@ -8,98 +8,115 @@ import {
 } from "graphql";
 import { globalIdField } from "graphql-relay";
 
-import { CommentLoader, DonationLoader, PredictionLoader, UserLoader, ChallengeCardLoader } from '../../loaders';
-import { GraphQLContext } from '../../TypeDefinition';
-import { connectionArgs, connectionDefinitions, mongooseIDResolver, withFilter } from '../../utils';
-import { ChallengeCardConnection } from '../challengecard/ChallengeCardType';
-import { CommentConnection } from '../comment/CommentType';
-import { DonationConnection } from '../donation/DonationType';
-import { nodeInterface, registerTypeLoader } from '../node/typeRegister';
-import { PredictionConnection } from '../prediction/PredictionType';
-import UserType from '../user/UserType';
-import { load } from './ChallengeLoader';
-import { IChallenge } from './ChallengeModel';
+import {
+  CommentLoader,
+  DonationLoader,
+  PredictionLoader,
+  UserLoader,
+  ChallengeCardLoader
+} from "../../loaders";
+import { GraphQLContext } from "../../TypeDefinition";
+import {
+  connectionArgs,
+  connectionDefinitions,
+  mongooseIDResolver,
+  withFilter
+} from "../../utils";
+import { ChallengeCardConnection } from "../challengecard/ChallengeCardType";
+import { CommentConnection } from "../comment/CommentType";
+import { DonationConnection } from "../donation/DonationType";
+import { nodeInterface, registerTypeLoader } from "../node/typeRegister";
+import { PredictionConnection } from "../prediction/PredictionType";
+import UserType from "../user/UserType";
+import { load } from "./ChallengeLoader";
+import { IChallenge } from "./ChallengeModel";
 
 const ChallengeType = new GraphQLObjectType<IChallenge, GraphQLContext>({
-	name: 'Challenge',
-	description: 'Challenge data',
-	fields: () => ({
-		id: globalIdField('Challenge'),
-		...mongooseIDResolver,
-		title: {
-			type: GraphQLNonNull(GraphQLString),
-			resolve: (challenge) => challenge.title,
-		},
-		content: {
-			type: GraphQLString,
-			resolve: (challenge) => challenge.content,
-		},
-		active: {
-			type: GraphQLNonNull(GraphQLBoolean),
-			resolve: (challenge) => challenge.active,
-		},
-		series: {
-			type: GraphQLNonNull(GraphQLInt),
-			resolve: (challenge) => challenge.series,
-		},
-		donations: {
-			type: DonationConnection.connectionType,
-			args: {
-				...connectionArgs,
-			},
-			resolve: async (challenge, args, context) => {
-				const donations = await DonationLoader.loadAll(context, withFilter(args, { challenge: challenge._id }));
-				return donations;
-			},
-		},
-		creator: {
-			type: GraphQLNonNull(UserType),
-			resolve: (challenge, _, context) => {
-				return UserLoader.load(context, challenge.creator);
-			},
-		},
-		options: {
-			type: GraphQLNonNull(GraphQLList(GraphQLString)),
-			resolve: (challenge) => challenge.options,
-		},
-		predictions: {
-			type: PredictionConnection.connectionType,
-			args: {
-				...connectionArgs,
-			},
-			resolve: async (challenge, args, context) => {
-				const predictions = await PredictionLoader.loadAll(
-					context,
-					withFilter(args, { challenge: challenge._id })
-				);
-				return predictions;
-			},
-		},
-		comments: {
-			type: CommentConnection.connectionType,
-			args: {
-				...connectionArgs,
-			},
-			resolve: async (challenge, args, context) => {
-				const comments = await CommentLoader.loadAll(context, withFilter(args, { challenge: challenge._id }));
-				return comments;
-			},
-		},
-		challengeCards: {
-			type: ChallengeCardConnection.connectionType,
-			args: {
-				...connectionArgs,
-			},
-			resolve: async (challenge, args, context) => {
-				const challengeCards = await ChallengeCardLoader.loadAll(
-					context,
-					withFilter(args, { challenge: challenge._id })
-				);
-				return challengeCards;
-			},
-		},
-	}),
-	interfaces: () => [nodeInterface],
+  name: "Challenge",
+  description: "Challenge data",
+  fields: () => ({
+    id: globalIdField("Challenge"),
+    ...mongooseIDResolver,
+    title: {
+      type: GraphQLNonNull(GraphQLString),
+      resolve: challenge => challenge.title
+    },
+    content: {
+      type: GraphQLString,
+      resolve: challenge => challenge.content
+    },
+    active: {
+      type: GraphQLNonNull(GraphQLBoolean),
+      resolve: challenge => challenge.active
+    },
+    series: {
+      type: GraphQLNonNull(GraphQLInt),
+      resolve: challenge => challenge.series
+    },
+    donations: {
+      type: DonationConnection.connectionType,
+      args: {
+        ...connectionArgs
+      },
+      resolve: async (challenge, args, context) => {
+        const donations = await DonationLoader.loadAll(
+          context,
+          withFilter(args, { challenge: challenge._id })
+        );
+        return donations;
+      }
+    },
+    creator: {
+      type: GraphQLNonNull(UserType),
+      resolve: (challenge, _, context) => {
+        return UserLoader.load(context, challenge.creator);
+      }
+    },
+    options: {
+      type: GraphQLNonNull(GraphQLList(GraphQLString)),
+      resolve: challenge => challenge.options
+    },
+    predictions: {
+      type: PredictionConnection.connectionType,
+      args: {
+        ...connectionArgs
+      },
+      resolve: async (challenge, args, context) => {
+        const predictions = await PredictionLoader.loadAll(
+          context,
+          withFilter(args, { challenge: challenge._id })
+        );
+        return predictions;
+      }
+    },
+    comments: {
+      type: CommentConnection.connectionType,
+      args: {
+        ...connectionArgs
+      },
+      resolve: async (challenge, args, context) => {
+        const comments = await CommentLoader.loadAll(
+          context,
+          withFilter(args, { challenge: challenge._id })
+        );
+        return comments;
+      }
+    },
+    challengeCards: {
+      type: ChallengeCardConnection.connectionType,
+      args: {
+        ...connectionArgs
+      },
+      resolve: async (challenge, args, context) => {
+        const challengeCards = await ChallengeCardLoader.loadAll(
+          context,
+          withFilter(args, { challenge: challenge._id })
+        );
+        return challengeCards;
+      }
+    }
+  }),
+  interfaces: () => [nodeInterface]
 });
 
 export { ChallengeType };
