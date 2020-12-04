4ef4ee56ad5e4763b73eb460445f0a3d724079e9
YoungKidWarrior <ginelliunit@gmail.com>
2020-12-04 13:47:41 -0800

    3box Schema Stitching merge 3box getBoxProfile with local schema

--- packages/server/src/graphql/schema/index.ts
+++ packages/server/src/graphql/schema/index.ts
@@ -1,11 +1,25 @@
-import { GraphQLSchema } from "graphql";
+import { GraphQLSchema } from 'graphql';
+import { stitchSchemas } from '@graphql-tools/stitch';
 
-import { MutationType } from "./type/MutationType";
-import { QueryType } from "./type/QueryType";
-import { SubscriptionType } from "./type/SubscriptionType";
+import { MutationType } from './type/MutationType';
+import { QueryType } from './type/QueryType';
+import { SubscriptionType } from './type/SubscriptionType';
 
-export const schema = new GraphQLSchema({
-  query: QueryType,
-  mutation: MutationType,
-  subscription: SubscriptionType
+import { remoteSchema } from '../../graphql/remote-schemas/schema';
+
+const localSchema = new GraphQLSchema({
+	query: QueryType,
+	mutation: MutationType,
+	subscription: SubscriptionType,
+});
+
+export const schema = stitchSchemas({
+	subschemas: [
+		{
+			schema: localSchema,
+		},
+		{
+			schema: remoteSchema,
+		},
+	],
 });
