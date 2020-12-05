d1784396b9c2704921058449f0e114cbdeea9428
YoungKidWarrior <ginelliunit@gmail.com>
2020-12-04 20:09:40 -0800

    Boiler Plate for Vote Module

---
+++ packages/server/src/graphql/modules/vote/VoteType.ts
@@ -0,0 +1,55 @@
+import {
+  GraphQLID,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLObjectType,
+} from "graphql";
+import { globalIdField } from "graphql-relay";
+
+import { ChallengeLoader, UserLoader } from "../../loaders";
+import { GraphQLContext } from "../../TypeDefinition";
+import { connectionDefinitions } from "../../utils";
+import { ChallengeType } from "../challenge/ChallengeType";
+import { nodeInterface, registerTypeLoader } from "../node/typeRegister";
+import UserType from "../user/UserType";
+import { load } from "./VoteLoader";
+import { IVote } from "./VoteModel";
+
+const VoteType = new GraphQLObjectType<IVote, GraphQLContext>({
+  name: "Vote",
+  description: "Vote data",
+  fields: () => ({
+    id: globalIdField("Vote"),
+    _id: {
+      type: GraphQLNonNull(GraphQLID),
+      resolve: (vote) => vote._id,
+    },
+    challenge: {
+      type: ChallengeType,
+      resolve: (vote, _, context) =>
+        ChallengeLoader.load(context, vote.challenge),
+    },
+    challengeSeries: {
+      type: GraphQLNonNull(GraphQLInt),
+      resolve: (vote) => vote.challengeSeries,
+    },
+    choice: {
+      type: GraphQLNonNull(GraphQLInt),
+      resolve: (vote) => vote.choice,
+    },
+    creator: {
+      type: GraphQLNonNull(UserType),
+      resolve: (vote, _, context) => UserLoader.load(context, vote.creator),
+    },
+  }),
+  interfaces: () => [nodeInterface],
+});
+
+export { VoteType };
+
+registerTypeLoader(VoteType, load);
+
+export const VoteConnection = connectionDefinitions({
+  name: "Vote",
+  nodeType: VoteType,
+});
--- packages/server/src/graphql/schema/type/MutationType.ts
+++ packages/server/src/graphql/schema/type/MutationType.ts
@@ -6,6 +6,7 @@ import * as CommentMutations from "../../modules/comment/mutations";
 import * as DonationMutations from "../../modules/donation/mutations";
 import PredictionMutations from "../../modules/prediction/mutations";
 import * as UserMutations from "../../modules/user/mutations";
+import * as VoteMutations from "../../modules/vote/mutations";
 
 export const MutationType = new GraphQLObjectType({
   name: "Mutation",
@@ -15,6 +16,7 @@ export const MutationType = new GraphQLObjectType({
     ...PredictionMutations,
     ...CommentMutations,
     ...DonationMutations,
-    ...ChallengeCardMutations
-  })
+    ...ChallengeCardMutations,
+    ...VoteMutations,
+  }),
 });
