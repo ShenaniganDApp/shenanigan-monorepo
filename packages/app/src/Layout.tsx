8f37baa265459068c1ecf16b6e98d2b1f89a996b
Tawnee Allport <allprt89@gmail.com>
2020-12-14 19:42:28 -0700

    add layout to app. remove unused imports

--- packages/app/src/Layout.tsx
+++ packages/app/src/Layout.tsx
@@ -3,7 +3,13 @@ import { Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Swiper from 'react-native-swiper';
 
-const Layout = ({ children }): React.ReactElement => (
+type Props = {
+  children?:
+    | React.ReactChild
+    | React.ReactChild[];
+};
+
+const Layout = ({ children }: Props): React.ReactElement => (
   <Swiper
       horizontal={false} 
       showsPagination={false}
