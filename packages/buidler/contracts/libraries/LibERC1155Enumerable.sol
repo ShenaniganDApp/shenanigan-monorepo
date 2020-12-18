0eb94f567f25f583811bf7300f9df73728a9e43d
YoungKidWarrior <ginelliunit@gmail.com>
2020-12-17 17:45:49 -0800

    ERC 1155 enumerable contracts

---
+++ packages/buidler/contracts/libraries/LibERC1155Enumerable.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.5;
+
+import '@openzeppelin/contracts/utils/EnumerableSet.sol';
+
+library LibERC1155Enumerable {
+  bytes32 internal constant STORAGE_SLOT = keccak256(
+    'solidstate.contracts.storage.ERC1155Enumerable'
+  );
+
+  struct Layout {
+    mapping (uint => uint) totalSupply;
+    mapping (uint => EnumerableSet.AddressSet) accountsByToken;
+    mapping (address => EnumerableSet.UintSet) tokensByAccount;
+  }
+
+  function layout () internal pure returns (Layout storage l) {
+    bytes32 slot = STORAGE_SLOT;
+    assembly { l.slot := slot }
+  }
+}
