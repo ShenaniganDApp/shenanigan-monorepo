038a3e1d60139abe8d9616e99ed8becc4bbc55ff
YoungKidWarrior <ginelliunit@gmail.com>
2021-01-17 09:09:18 -0800

    Pull active challenges from backend and display them in Lineup Tab

---
+++ packages/app/src/components/lineup/LineupList.tsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import { FlatList, Text, TouchableHighlight, View } from 'react-native';
+import { graphql } from 'react-relay';
+import { usePagination } from 'relay-hooks';
+import { LineupListPaginationQueryVariables } from './__generated__/LineupListPaginationQuery.graphql';
+
+import {
+    LineupList_query,
+    LineupList_query$key
+} from './__generated__/LineupList_query.graphql';
+
+const lineupFragmentSpec = graphql`
+    fragment LineupList_query on Query
+    @argumentDefinitions(
+        count: { type: "Int", defaultValue: 20 }
+        cursor: { type: "String" }
+    ) {
+        activeChallenges(first: $count, after: $cursor)
+            @connection(key: "LineupList_activeChallenges", filters: []) {
+            endCursorOffset
+            startCursorOffset
+            count
+            pageInfo {
+                hasNextPage
+                hasPreviousPage
+                startCursor
+                endCursor
+            }
+            edges {
+                node {
+                    id
+                    _id
+                    title
+                    active
+                }
+            }
+        }
+    }
+`;
+
+const connectionConfig = {
+    getVariables(props: LineupList_query, { count, cursor }) {
+        return {
+            count,
+            cursor
+        };
+    },
+    query: graphql`
+        # Pagination query to be fetched upon calling 'loadMore'.
+        # Notice that we re-use our fragment, and the shape of this query matches our fragment spec.
+        query LineupListPaginationQuery($count: Int!, $cursor: String) {
+            ...LineupList_query @arguments(count: $count, cursor: $cursor)
+        }
+    `
+};
+
+type Props = {
+    query: LineupList_query$key;
+};
+
+export const LineupList = (props: Props) => {
+    const [isFetchingTop, setIsFetchingTop] = useState(false);
+    const [
+        query,
+        { isLoading, hasMore, loadMore, refetchConnection }
+    ] = usePagination(lineupFragmentSpec, props.query);
+    const { activeChallenges } = query;
+    const refetchList = () => {
+        if (isLoading()) {
+            return;
+        }
+        setIsFetchingTop(true);
+        refetchConnection(
+            connectionConfig,
+            10, // Fetch the next 10 feed items
+            (error) => {
+                setIsFetchingTop(false);
+                console.log(error);
+            }
+        );
+    };
+    const loadNext = () => {
+        if (!hasMore() || isLoading()) {
+            return;
+        }
+
+        loadMore(
+            connectionConfig,
+            10, // Fetch the next 10 feed items
+            (error) => {
+                console.log(error);
+            }
+        );
+    };
+    return (
+        //@TODO handle null assertions
+        <FlatList
+            nestedScrollEnabled={true}
+            style={{ backgroundColor: '#e6ffff' }}
+            data={activeChallenges.edges}
+            renderItem={({ item }) => {
+                if (!item) return <Text>Not Here</Text>;
+                const { node } = item;
+
+                return (
+                    <TouchableHighlight underlayColor="whitesmoke">
+                        <View>
+                            <Text>{node.title}</Text>
+                            <Text>{node.active.toString()}</Text>
+                        </View>
+                    </TouchableHighlight>
+                );
+            }}
+            keyExtractor={(item) => item.node._id}
+            onEndReached={loadNext}
+            onRefresh={refetchList}
+            refreshing={isFetchingTop}
+            ItemSeparatorComponent={() => <View style={null} />}
+            ListFooterComponent={null}
+        />
+    );
+};
