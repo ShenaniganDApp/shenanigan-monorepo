a674837733dc1cb89f0f7a6e8a3a76502f5b6295
Tawnee Allport <allprt89@gmail.com>
2021-01-16 18:32:42 -0700

    testing swiper

--- packages/app/src/Layout.tsx
+++ packages/app/src/Layout.tsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Swiper from 'react-native-swiper';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { LiveTabProps, LiveTabs } from './Navigator';
+import { useFocusEffect } from '@react-navigation/native';
 
 type Props = {
-  children?:
-    | React.ReactChild
-    | React.ReactChild[];
+    children?: React.ReactChild | React.ReactChild[];
 };
 
-const Layout = ({ children }: Props): React.ReactElement => (
-  <Swiper
-      horizontal={false} 
-      showsPagination={false}
-      loop={false}
-      index={1}
-    >
-      {children}
-    </Swiper>
-)
+const Layout = ({
+    children,
+    bottom,
+    me,
+    liveChallenge
+}: Props): React.ReactElement => {
+    const [index, setIndex] = useState(1);
+    const refContainer = useRef(null);
+
+    useFocusEffect(
+        React.useCallback(() => {
+            // Do something when the screen is focused
+            console.log('swiper');
+            refContainer.current.scrollBy(1);
+            setIndex(1);
+            return () => {
+                // Do something when the screen is unfocused
+                // Useful for cleanup functions
+            };
+        }, [])
+    );
+
+    return (
+        <Swiper
+            horizontal={false}
+            showsPagination={false}
+            loop={false}
+            index={index}
+            onIndexChanged={(index) => console.log(index)}
+            ref={refContainer}
+        >
+            <SafeAreaView>
+                <Text>top</Text>
+            </SafeAreaView>
+            {children}
+            {bottom && (
+                <SafeAreaView style={{ height: '100%' }}>
+                    <LiveTabs me={me} liveChallenge={liveChallenge} />
+                </SafeAreaView>
+            )}
+        </Swiper>
+    );
+};
 
 export default Layout;
--- packages/app/src/components/market/Market.tsx
+++ packages/app/src/components/market/Market.tsx
@@ -4,6 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { useQuery, graphql } from 'relay-hooks';
 import { MarketList } from './MarketList';
 import { MarketQuery } from './__generated__/MarketQuery.graphql';
+import Layout from '../../Layout';
+import { useFocusEffect } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
     background: { backgroundColor: '#e6ffff' }
@@ -17,15 +19,30 @@ const query = graphql`
 
 export const Market = (): React.ReactElement => {
     const { props: data, retry } = useQuery<MarketQuery>(query);
+    useFocusEffect(
+        React.useCallback(() => {
+            // Do something when the screen is focused
+            console.log('market');
+
+            return () => {
+                // Do something when the screen is unfocused
+                // Useful for cleanup functions
+            };
+        }, [])
+    );
 
     return data ? (
-        <SafeAreaView style={styles.background}>
-            <Text>Market</Text>
-            <MarketList query={data} />
-        </SafeAreaView>
+        <Layout>
+            <SafeAreaView style={styles.background}>
+                <Text>Market</Text>
+                <MarketList query={data} />
+            </SafeAreaView>
+        </Layout>
     ) : (
-        <SafeAreaView>
-            <Button title={'Retry'} onPress={retry} />
-        </SafeAreaView>
+        <Layout>
+            <SafeAreaView>
+                <Button title={'Retry'} onPress={retry} />
+            </SafeAreaView>
+        </Layout>
     );
 };
