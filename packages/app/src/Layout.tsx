e9734aeb49b8021ad4505ac7af70ba215c402c91
Tawnee Allport <allprt89@gmail.com>
2021-01-16 19:12:36 -0700

    add hoc

--- packages/app/src/Layout.tsx
+++ packages/app/src/Layout.tsx
@@ -1,56 +1,38 @@
-import React, { useEffect, useState, useRef } from 'react';
-import Swiper from 'react-native-swiper';
-import { View, Text } from 'react-native';
+import React, { ComponentType } from 'react';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { LiveTabProps, LiveTabs } from './Navigator';
-import { useFocusEffect } from '@react-navigation/native';
+import Swiper from 'react-native-swiper';
+import { LiveTabs } from './Navigator';
 
-type Props = {
-    children?: React.ReactChild | React.ReactChild[];
-};
-
-const Layout = ({
-    children,
-    bottom,
-    me,
-    liveChallenge
-}: Props): React.ReactElement => {
-    const [index, setIndex] = useState(1);
-    const refContainer = useRef(null);
-
-    useFocusEffect(
-        React.useCallback(() => {
-            // Do something when the screen is focused
-            console.log('swiper');
-            refContainer.current.scrollBy(1);
-            setIndex(1);
-            return () => {
-                // Do something when the screen is unfocused
-                // Useful for cleanup functions
-            };
-        }, [])
-    );
-
-    return (
-        <Swiper
-            horizontal={false}
-            showsPagination={false}
-            loop={false}
-            index={index}
-            onIndexChanged={(index) => console.log(index)}
-            ref={refContainer}
-        >
-            <SafeAreaView>
-                <Text>top</Text>
-            </SafeAreaView>
-            {children}
-            {bottom && (
-                <SafeAreaView style={{ height: '100%' }}>
-                    <LiveTabs me={me} liveChallenge={liveChallenge} />
+function withLayout<T>(
+    WrappedComponent: ComponentType<T>,
+    hasLiveTabs?: boolean
+) {
+    const HOC = (props: T) => {
+        return (
+            <Swiper
+                horizontal={false}
+                showsPagination={false}
+                loop={false}
+                index={1}
+            >
+                <SafeAreaView>
+                    <Text>Top!</Text>
                 </SafeAreaView>
-            )}
-        </Swiper>
-    );
-};
 
-export default Layout;
+                <View style={{ height: '100%' }}>
+                    <WrappedComponent {...props} />
+                </View>
+
+                {hasLiveTabs && (
+                    <SafeAreaView style={{ height: '100%' }}>
+                        <LiveTabs />
+                    </SafeAreaView>
+                )}
+            </Swiper>
+        );
+    };
+    return HOC;
+}
+
+export default withLayout;
