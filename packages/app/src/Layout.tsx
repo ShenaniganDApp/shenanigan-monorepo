b5b1c18d4aa4b49dc0c65f1454d20cf806fb2e77
Tawnee Allport <allprt89@gmail.com>
2021-01-17 19:19:53 -0700

    remove hoc

--- packages/app/src/Layout.tsx
+++ packages/app/src/Layout.tsx
@@ -4,35 +4,18 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Swiper from 'react-native-swiper';
 import { LiveTabs } from './Navigator';
 
-function withLayout<T>(
-    WrappedComponent: ComponentType<T>,
-    hasLiveTabs?: boolean
-) {
-    const HOC = (props: T) => {
-        return (
-            <Swiper
-                horizontal={false}
-                showsPagination={false}
-                loop={false}
-                index={1}
-            >
-                <SafeAreaView>
-                    <Text>Top!</Text>
-                </SafeAreaView>
+export const Layout = ({ children, hasLiveTabs, me, liveChallenge }) => (
+    <Swiper horizontal={false} showsPagination={false} loop={false} index={1}>
+        <SafeAreaView>
+            <Text>Top!</Text>
+        </SafeAreaView>
 
-                <View style={{ height: '100%' }}>
-                    <WrappedComponent {...props} />
-                </View>
+        {children}
 
-                {hasLiveTabs && (
-                    <SafeAreaView style={{ height: '100%' }}>
-                        <LiveTabs />
-                    </SafeAreaView>
-                )}
-            </Swiper>
-        );
-    };
-    return HOC;
-}
-
-export default withLayout;
+        {hasLiveTabs && (
+            <SafeAreaView style={{ height: '100%' }}>
+                <LiveTabs me={me} liveChallenge={liveChallenge} />
+            </SafeAreaView>
+        )}
+    </Swiper>
+);
