5955e173e1d33aacab622d73ed580c5a3a3698dc
YoungKidWarrior <ginelliunit@gmail.com>
2021-01-23 01:56:47 -0700

    Display user challenges in a list on profile

--- packages/app/src/components/challenges/mutations/CreateChallengeMutation.tsx
+++ packages/app/src/components/challenges/mutations/CreateChallengeMutation.tsx
@@ -1,5 +1,10 @@
 import { graphql } from 'react-relay';
-import { ConnectionHandler, RecordSourceSelectorProxy, ROOT_ID, SelectorStoreUpdater } from 'relay-runtime';
+import {
+    ConnectionHandler,
+    RecordSourceSelectorProxy,
+    ROOT_ID,
+    SelectorStoreUpdater
+} from 'relay-runtime';
 import { connectionUpdater } from '../../../relay';
 import { CreateChallengeInput } from './__generated__/CreateChallengeMutation.graphql';
 
@@ -29,41 +34,57 @@ export const updater = (parentId: string): SelectorStoreUpdater => (
     const root = store.getRootField('CreateChallenge');
     if (root) {
         const newEdge = root.getLinkedRecord('challengeEdge');
-        newEdge
-            ? connectionUpdater({
-                  store,
-                  parentId,
-                  connectionName: 'LineupList_activeChallenges',
-                  edge: newEdge,
-                  before: true
-              })
-            : null;
+        if (newEdge) {
+            connectionUpdater({
+                store,
+                parentId,
+                connectionName: 'LineupList_activeChallenges',
+                edge: newEdge,
+                before: true
+            });
+            connectionUpdater({
+                store,
+                parentId,
+                connectionName: 'UserChallengesList_createdChallenges',
+                edge: newEdge,
+                before: true
+            });
+        }
         //@TODO handle error
     }
 };
 
 const tempID = 0;
-export const optimisticUpdater = (
-    input: CreateChallengeInput,
-) => (store: RecordSourceSelectorProxy) => {
+export const optimisticUpdater = (input: CreateChallengeInput) => (
+    store: RecordSourceSelectorProxy
+) => {
     const id = 'client:newChallenge:' + tempID + 1;
 
-    const node = store.create(id, 'Comment');
+    const node = store.create(id, 'Challenge');
 
     node.setValue(id, 'id');
     node.setValue(input.address, 'address');
-    node.setValue(input.title, "title")
-    node.setValue(input.content,"content")
-    node.setValue(input.positiveOptions, "positiveOptions")
-    node.setValue(input.negativeOptions, "negativeOptions")
+    node.setValue(input.title, 'title');
+    node.setValue(input.content, 'content');
+    node.setValue(input.positiveOptions, 'positiveOptions');
+    node.setValue(input.negativeOptions, 'negativeOptions');
 
-    const newEdge = store.create('client:newEdge:' + tempID + 1, 'ChallengeEdge');
+    const newEdge = store.create(
+        'client:newEdge:' + tempID + 1,
+        'ChallengeEdge'
+    );
     newEdge.setLinkedRecord(node, 'node');
 
     const parentProxy = store.get(ROOT_ID);
-    const conn = ConnectionHandler.getConnection(
+    const lineupConn = ConnectionHandler.getConnection(
         parentProxy,
         'LineupList_challenges'
     );
-    ConnectionHandler.insertEdgeBefore(conn, newEdge);
+    ConnectionHandler.insertEdgeBefore(lineupConn, newEdge);
+
+    const userChallengesConn = ConnectionHandler.getConnection(
+        parentProxy,
+        'UserChallengesList_createdChallenges'
+    );
+    ConnectionHandler.insertEdgeBefore(userChallengesConn, newEdge);
 };
