4a4346c2649557cde40ac868ec19bb8e0ecc82d7
Tawnee <allprt89@gmail.com>
2021-02-07 13:12:08 -0700

    add basic card styles

--- packages/app/src/components/lineup/Lineup.tsx
+++ packages/app/src/components/lineup/Lineup.tsx
@@ -1,14 +1,11 @@
 import React, { ReactElement } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, View, Text } from 'react-native';
 import { graphql, useQuery } from 'relay-hooks';
 
 import { LineupQuery } from './__generated__/LineupQuery.graphql';
 import { LineupList } from './LineupList';
 import { LineupProps as Props } from '../../Navigator';
-
-const styles = StyleSheet.create({
-    background: { backgroundColor: '#e6ffff', height: '100%' }
-});
+import LinearGradient from 'react-native-linear-gradient';
 
 const query = graphql`
     query LineupQuery {
@@ -20,11 +17,43 @@ export const Lineup = (props: Props): ReactElement => {
     // @TODO handle error
     const { props: data, retry } = useQuery<LineupQuery>(query);
 
-    return data ? (
-        <View style={styles.background}>
+    // return data ? (
+    //     <LinearGradient
+    //         colors={['#FF016D', '#E6FFFF']}
+    //         style={styles.background}
+    //     >
+    //         <Text style={styles.title}>Line Up</Text>
+    //         <LineupList query={data} />
+    //     </LinearGradient>
+    // ) : (
+    //     <Button title="Retry" onPress={retry} />
+    // );
+
+    return (
+        <LinearGradient
+            colors={['#FF016D', '#E6FFFF']}
+            style={styles.background}
+        >
+            <Text style={styles.title}>Line Up</Text>
             <LineupList query={data} />
-        </View>
-    ) : (
-        <Button title="Retry" onPress={retry} />
+        </LinearGradient>
     );
 };
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        paddingLeft: 15,
+        paddingRight: 15
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 32,
+        textAlign: 'center',
+        marginVertical: 12,
+        textTransform: 'uppercase',
+        textShadowColor: 'rgba(255, 255, 255, 0.8)',
+        textShadowOffset: { width: 0, height: 0 },
+        textShadowRadius: 10
+    }
+});
--- packages/app/src/components/lineup/LineupList.tsx
+++ packages/app/src/components/lineup/LineupList.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { FlatList, Text, TouchableHighlight, View } from 'react-native';
+import {
+    FlatList,
+    Text,
+    TouchableHighlight,
+    View,
+    StyleSheet
+} from 'react-native';
 import { graphql } from 'react-relay';
 import { usePagination } from 'relay-hooks';
 import { LineupListPaginationQueryVariables } from './__generated__/LineupListPaginationQuery.graphql';
@@ -11,10 +17,10 @@ import {
 
 const lineupFragmentSpec = graphql`
     fragment LineupList_query on Query
-    @argumentDefinitions(
-        count: { type: "Int", defaultValue: 20 }
-        cursor: { type: "String" }
-    ) {
+        @argumentDefinitions(
+            count: { type: "Int", defaultValue: 20 }
+            cursor: { type: "String" }
+        ) {
         activeChallenges(first: $count, after: $cursor)
             @connection(key: "LineupList_activeChallenges", filters: []) {
             endCursorOffset
@@ -58,6 +64,69 @@ type Props = {
     query: LineupList_query$key;
 };
 
+const testData = [
+    {
+        cursor: 'bW9uZ286MA==',
+        node: {
+            __typename: 'Challenge',
+            _id: '601b213734494a19ced7a2ef',
+            active: true,
+            id: 'Q2hhbGxlbmdlOjYwMWIyMTM3MzQ0OTRhMTljZWQ3YTJlZg==',
+            title: 'New'
+        }
+    },
+    {
+        cursor: 'bW9uZ286MQ==',
+        node: {
+            __typename: 'Challenge',
+            _id: '600bd2b0751b7f72edeae118',
+            active: true,
+            id: 'Q2hhbGxlbmdlOjYwMGJkMmIwNzUxYjdmNzJlZGVhZTExOA==',
+            title: 'Aacacs'
+        }
+    },
+    {
+        cursor: 'bW9uZ286Mg==',
+        node: {
+            __typename: 'Challenge',
+            _id: '5ffce55c6e8fa0f177dd0fe6',
+            active: true,
+            id: 'Q2hhbGxlbmdlOjVmZmNlNTVjNmU4ZmEwZjE3N2RkMGZlNg==',
+            title: 'Iddi'
+        }
+    },
+    {
+        cursor: 'bW9uZ286MA==',
+        node: {
+            __typename: 'Challenge',
+            _id: '601b213734494a19ced7a2ef',
+            active: true,
+            id: 'Q2hhbGxlbmdlOjYwMWIyMTM3MzQ0OTRhMTljZWQ3YTJlZg==',
+            title: 'New'
+        }
+    },
+    {
+        cursor: 'bW9uZ286MQ==',
+        node: {
+            __typename: 'Challenge',
+            _id: '600bd2b0751b7f72edeae118',
+            active: true,
+            id: 'Q2hhbGxlbmdlOjYwMGJkMmIwNzUxYjdmNzJlZGVhZTExOA==',
+            title: 'Aacacs'
+        }
+    },
+    {
+        cursor: 'bW9uZ286Mg==',
+        node: {
+            __typename: 'Challenge',
+            _id: '5ffce55c6e8fa0f177dd0fe6',
+            active: true,
+            id: 'Q2hhbGxlbmdlOjVmZmNlNTVjNmU4ZmEwZjE3N2RkMGZlNg==',
+            title: 'Iddi'
+        }
+    }
+];
+
 export const LineupList = (props: Props) => {
     const [isFetchingTop, setIsFetchingTop] = useState(false);
     const [
@@ -73,7 +142,7 @@ export const LineupList = (props: Props) => {
         refetchConnection(
             connectionConfig,
             10, // Fetch the next 10 feed items
-            (error) => {
+            error => {
                 setIsFetchingTop(false);
                 console.log(error);
             }
@@ -87,31 +156,48 @@ export const LineupList = (props: Props) => {
         loadMore(
             connectionConfig,
             10, // Fetch the next 10 feed items
-            (error) => {
+            error => {
                 console.log(error);
             }
         );
     };
+    console.log(activeChallenges.edges);
+
     return (
         //@TODO handle null assertions
         <FlatList
             nestedScrollEnabled={true}
-            style={{ backgroundColor: '#e6ffff' }}
-            data={activeChallenges.edges}
-            renderItem={({ item }) => {
+            // data={activeChallenges.edges}
+            data={testData}
+            renderItem={({ item, index }) => {
                 if (!item) return <Text>Not Here</Text>;
                 const { node } = item;
 
                 return (
                     <TouchableHighlight underlayColor="whitesmoke">
-                        <View>
-                            <Text>{node.title}</Text>
-                            <Text>{node.active.toString()}</Text>
+                        <View style={styles.card}>
+                            <View style={styles.donationContainer}>
+                                <Text style={styles.donation}>10 XDai</Text>
+                            </View>
+
+                            <View style={styles.cardInner}>
+                                <View style={styles.profile}>
+                                    <View style={styles.image} />
+                                    <Text style={styles.title}>Username</Text>
+                                </View>
+
+                                <Text>
+                                    {node.title} Lorem ipsum dolor, sit amet
+                                    consectetur adipisicing elit. Veritatis
+                                    delectus ad dignissimos?
+                                </Text>
+                            </View>
+                            {/* <Text>{node.active.toString()}</Text> */}
                         </View>
                     </TouchableHighlight>
                 );
             }}
-            keyExtractor={(item) => item.node._id}
+            keyExtractor={item => item.node._id}
             onEndReached={loadNext}
             onRefresh={refetchList}
             refreshing={isFetchingTop}
@@ -120,3 +206,45 @@ export const LineupList = (props: Props) => {
         />
     );
 };
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: '#E6FFFF',
+        borderRadius: 12,
+        marginBottom: 20,
+        borderWidth: 3,
+        borderColor: '#80FFFF',
+        overflow: 'hidden'
+    },
+    donationContainer: {
+        backgroundColor: '#80FFFF',
+        paddingVertical: 6,
+        paddingHorizontal: 12
+    },
+    donation: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontSize: 16
+    },
+    cardInner: {
+        paddingVertical: 12,
+        paddingHorizontal: 12
+    },
+    profile: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 8
+    },
+    image: {
+        height: 40,
+        width: 40,
+        backgroundColor: 'hotpink',
+        borderRadius: 20
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginLeft: 8
+    }
+});
