5ac21af68d415ba40751b31a8f0cdb4fd0676a71
Tawnee <allprt89@gmail.com>
2021-02-08 11:37:36 -0700

    add button and globalStyles

---
+++ packages/app/src/components/UI/Button.tsx
@@ -0,0 +1,67 @@
+import React, { ReactElement } from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { colors } from '../../globalStyles';
+
+export const Button = ({
+    color,
+    small,
+    shadow,
+    title,
+    onPress
+}): ReactElement => {
+    let bgColor;
+
+    if (color === 'yellow') {
+        bgColor = colors.yellow;
+    } else if (color === 'green') {
+        bgColor = colors.green;
+    } else {
+        bgColor = colors.pink;
+    }
+
+    return (
+        <TouchableOpacity
+            style={{
+                ...styles.button,
+                backgroundColor: bgColor,
+                shadowOpacity: shadow ? 0.2 : 0,
+                padding: small ? 10 : 14,
+                maxWidth: small ? 180 : 200
+            }}
+            onPress={onPress}
+            activeOpacity={0.8}
+        >
+            <Text
+                style={{
+                    ...styles.text,
+                    color: color === 'yellow' ? '#111' : '#FFF'
+                }}
+            >
+                {title}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 14,
+        width: '100%',
+        maxWidth: 200,
+        borderRadius: 10,
+        shadowColor: '#000',
+        alignSelf: 'center',
+        shadowOffset: {
+            width: 0,
+            height: 1
+        },
+        shadowRadius: 2,
+
+        elevation: 3
+    },
+    text: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 16
+    }
+});
