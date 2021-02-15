4d3b5fe4c1990c30107c9d151abc7839c9dd5f10
YoungKidWarrior <ginelliunit@gmail.com>
2021-02-14 18:14:38 -0800

    Mutation to Add and remove a single moderator from the user fields

---
+++ packages/server/src/graphql/modules/user/mutations/AddOrRemoveModerator.ts
@@ -0,0 +1,61 @@
+import { GraphQLNonNull, GraphQLString } from "graphql";
+import { mutationWithClientMutationId } from "graphql-relay";
+
+import { UserLoader } from "../../../loaders";
+import { GraphQLContext } from "../../../TypeDefinition";
+import UserModel from "../UserModel";
+import UserType from "../UserType";
+
+export const AddOrRemoveModerator = mutationWithClientMutationId({
+  name: "AddOrRemoveModerator",
+  inputFields: {
+    _id: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  mutateAndGetPayload: async ({ _id }, { user }: GraphQLContext) => {
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+    try {
+      const mod = await UserModel.findOne({ _id });
+      if (!mod) {
+        throw new Error("User attempted to mod does not exist");
+      }
+      const modIndex = user.moderatedUsers.indexOf(_id);
+      if (modIndex === -1) {
+        user.moderatedUsers.push(mod._id);
+        mod.moderator.push(mod._id);
+      } else {
+        mod.moderator.splice(modIndex, 1);
+
+        user.moderatedUsers.splice(modIndex, 1);
+      }
+      const userResult = await user.save();
+      const modResult = await mod.save();
+      return { userId: userResult._id, modId: modResult._id };
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+  outputFields: {
+    user: {
+      type: UserType,
+      resolve: async ({ userId }, _, context) => {
+        const user = await UserLoader.load(context, userId);
+        return user;
+      },
+    },
+    moderator: {
+      type: UserType,
+      resolve: async ({ modId }, _, context) => {
+        const user = await UserLoader.load(context, modId);
+        return user;
+      },
+    },
+    error: {
+      type: GraphQLString,
+      resolve: ({ error }) => error,
+    },
+  },
+});
