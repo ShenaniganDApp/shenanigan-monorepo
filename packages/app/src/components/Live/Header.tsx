5453462bcaf638b421c5ddc08730266d7115fa59
Tawnee <allprt89@gmail.com>
2021-03-01 13:42:39 -0700

    move fade into components to fix keyboardavoidingview

--- packages/app/src/components/Live/Header.tsx
+++ packages/app/src/components/Live/Header.tsx
@@ -8,13 +8,17 @@ type Props = {
     isMuted: boolean;
     setIsPaused: (b: boolean) => void;
     setIsMuted: (b: boolean) => void;
+    animationEvent: boolean;
+    animationHandler: () => void;
 };
 
 export const Header = ({
     isPaused,
     setIsPaused,
     isMuted,
-    setIsMuted
+    setIsMuted,
+    animationEvent,
+    animationHandler
 }: Props): ReactElement => {
     const [animation, setAnimation] = useState(false);
     const [visible, setVisible] = useState(false);
@@ -27,35 +31,37 @@ export const Header = ({
     };
 
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <View style={styles.infoContainer}>
-                    <View style={styles.image} />
-                    <Text style={styles.title}>
-                        This is the title of the stream
-                    </Text>
+        <Fade event={animationEvent} afterAnimationOut={animationHandler} down>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <View style={styles.infoContainer}>
+                        <View style={styles.image} />
+                        <Text style={styles.title}>
+                            This is the title of the stream
+                        </Text>
+                    </View>
+
+                    <Dots onPress={handlePress} />
                 </View>
+                {visible && (
+                    <Fade
+                        event={animation}
+                        style={styles.buttonContainer}
+                        afterAnimationOut={() => setVisible(false)}
+                    >
+                        <PlayControls
+                            onPress={() => setIsPaused(!isPaused)}
+                            isPaused={isPaused}
+                        />
 
-                <Dots onPress={handlePress} />
+                        <VolumeControls
+                            onPress={() => setIsMuted(!isMuted)}
+                            isMuted={isMuted}
+                        />
+                    </Fade>
+                )}
             </View>
-            {visible && (
-                <Fade
-                    event={animation}
-                    style={styles.buttonContainer}
-                    afterAnimationOut={() => setVisible(false)}
-                >
-                    <PlayControls
-                        onPress={() => setIsPaused(!isPaused)}
-                        isPaused={isPaused}
-                    />
-
-                    <VolumeControls
-                        onPress={() => setIsMuted(!isMuted)}
-                        isMuted={isMuted}
-                    />
-                </Fade>
-            )}
-        </View>
+        </Fade>
     );
 };
 
