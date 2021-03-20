b5b3115f41c5f9b1961e41b936df42384c2e41e7
YoungKidWarrior <ginelliunit@gmail.com>
2021-03-19 23:40:21 -0700

    Start challenge result subscription

--- packages/server/src/graphql/schema/type/SubscriptionType.ts
+++ packages/server/src/graphql/schema/type/SubscriptionType.ts
@@ -1,6 +1,6 @@
 import { GraphQLObjectType } from 'graphql';
 
-import { ChallengeAdded } from '../../modules/challenge/subscriptions';
+import { ChallengeAdded, ChallengeResultStarted } from '../../modules/challenge/subscriptions';
 import { CommentAdded } from '../../modules/comment/subscriptions';
 import { VoteAdded } from '../../modules/vote/subscriptions';
 
@@ -8,6 +8,7 @@ export const SubscriptionType = new GraphQLObjectType({
 	name: 'Subscription',
 	fields: {
 		ChallengeAdded: ChallengeAdded as any,
+		ChallengeResultStarted: ChallengeResultStarted as any,
 		CommentAdded: CommentAdded as any,
 		VoteAdded: VoteAdded as any,
 	},
