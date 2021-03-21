82826d50c359549b6b4c3856d25c4ba690bb241a
Tawnee <allprt89@gmail.com>
2021-03-21 17:38:38 -0600

    remove livetabs header. fix comments keyboard offset.

--- packages/app/src/Navigator.tsx
+++ packages/app/src/Navigator.tsx
@@ -194,6 +194,7 @@ export function LiveTabs({
             onIndexChange={setIndex}
             position={position}
             swipeEnabled={canSwipe}
+            renderTabBar={() => <></>}
         />
     );
 }
