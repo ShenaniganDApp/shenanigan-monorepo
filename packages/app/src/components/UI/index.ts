9ebdcf247b244bdad749383411e82b91e9d00201
Tawnee <allprt89@gmail.com>
2021-03-21 14:20:06 -0600

    add outcome video controls. breakout components into separate files.

--- packages/app/src/components/UI/index.ts
+++ packages/app/src/components/UI/index.ts
@@ -2,6 +2,7 @@ import Button from './Button';
 import Card from './Card';
 import Fade from './Fade';
 import VideoPlayer from './Video';
+import LoadingSpinner from './LoadingSpinner';
 import { colors } from './globalStyles';
 
-export { Button, Card, Fade, VideoPlayer as Video, colors };
+export { Button, Card, Fade, VideoPlayer as Video, LoadingSpinner, colors };
