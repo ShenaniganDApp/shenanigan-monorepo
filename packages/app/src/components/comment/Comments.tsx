09a859b0059db0e60f7cbf3c34f2902910b4e14c
Tawnee <allprt89@gmail.com>
2021-03-22 09:55:52 -0600

    finish comments list styles. add header to list

--- packages/app/src/components/comment/Comments.tsx
+++ packages/app/src/components/comment/Comments.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { graphql, useFragment } from 'relay-hooks';
 
 import { CommentList } from './CommentList';
@@ -8,9 +8,6 @@ import { ChatProps as Props } from '../../Navigator';
 import { Comments_me$key } from './__generated__/Comments_me.graphql';
 import { useCommentAddedSubscription } from '../../hooks/useCommentAddedSubscription';
 import { Comments_liveChallenge$key } from './__generated__/Comments_liveChallenge.graphql';
-
-import { Card, colors } from '../UI';
-import { Blockie } from '../Web3';
 
 export const Comments = (props: Props): React.ReactElement => {
     // @TODO handle error
@@ -42,42 +39,11 @@ export const Comments = (props: Props): React.ReactElement => {
 
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Card
-                    bgColor="#FCFBC1"
-                    shadowColor="rgba(0,0,0,.2)"
-                    style={styles.card}
-                >
-                    <Text style={styles.title}>Top Donators:</Text>
-                    <View style={styles.topContainer}>
-                        {[1, 2, 3].map((n, i) => (
-                            <View
-                                style={[
-                                    styles.topImageBg,
-                                    { transform: [{ translateX: -10 * i }] }
-                                ]}
-                                key={i}
-                            >
-                                <Blockie
-                                    address={
-                                        '0x9d69631bdeeB04bAC2AC64C2C96aDD63079CB1f' +
-                                        n
-                                    }
-                                    size={10}
-                                    scale={4}
-                                />
-                            </View>
-                        ))}
-                    </View>
-                </Card>
-            </View>
-            <View style={styles.listContainer}>
-                <CommentList
-                    query={props.commentsQuery}
-                    chatScroll={props.chatScroll}
-                    setWalletScroll={props.setWalletScroll}
-                />
-            </View>
+            <CommentList
+                query={props.commentsQuery}
+                chatScroll={props.chatScroll}
+                setWalletScroll={props.setWalletScroll}
+            />
 
             <CreateCommentComposer me={me} liveChallenge={liveChallenge} />
         </View>
@@ -85,43 +51,5 @@ export const Comments = (props: Props): React.ReactElement => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    header: {
-        padding: 16
-    },
-    card: {
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 24,
-        textTransform: 'uppercase',
-        marginRight: 16
-    },
-    topContainer: {
-        flexDirection: 'row'
-    },
-    topImageBg: {
-        padding: 3,
-        backgroundColor: '#F0F0F0',
-        borderRadius: 6,
-        shadowColor: 'black',
-        shadowOpacity: 0.2,
-        shadowOffset: {
-            width: 0,
-            height: 1
-        },
-        shadowRadius: 3,
-        elevation: 2
-    },
-    listContainer: {
-        backgroundColor: colors.altWhite,
-        marginHorizontal: 16,
-        borderRadius: 6,
-        flex: 1,
-        flexGrow: 1
-    }
+    container: { flex: 1 }
 });
