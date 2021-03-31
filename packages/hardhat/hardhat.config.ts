95dcb8f6e4564f3ccd20e156c7e2b54b29edb785
Ed Zynda <ezynda3@gmail.com>
2021-03-31 10:47:15 +0700

    add task stubs

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,7 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
+import "@nomiclabs/hardhat-web3";
 
+import fs from "fs";
+import { task } from "hardhat/config";
 import { HardhatUserConfig } from "hardhat/types";
 
 const config: HardhatUserConfig = {
@@ -15,5 +18,56 @@ const config: HardhatUserConfig = {
     sources: "src",
   },
 };
+
+task("generate", "Create a mnemonic for hardhat deploys", async () => {
+  // TODO
+});
+
+task(
+  "account",
+  "Get balance informations for the deployment account.",
+  async () => {
+    // TODO
+  }
+);
+
+task(
+  "select",
+  "Activate one of the deploying accounts (just switchs mnemonic files around)"
+)
+  .addPositionalParam(
+    "address",
+    "The account's address. (should be an *address*.txt file here already from the `generate` task)"
+  )
+  .setAction(async (taskArgs) => {
+    // TODO
+  });
+
+task("accounts", "Prints the list of accounts", async () => {
+  // TODO
+});
+
+task("blockNumber", "Prints the block number", async () => {
+  // TODO
+});
+
+task("balance", "Prints an account's balance")
+  .addPositionalParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    // TODO
+  });
+
+task("send", "Send ETH")
+  .addParam("from", "From address or account index")
+  .addOptionalParam("to", "To address or account index")
+  .addOptionalParam("amount", "Amount to send in ether")
+  .addOptionalParam("data", "Data included in transaction")
+  .addOptionalParam("gasPrice", "Price you are willing to pay in gwei")
+  .addOptionalParam("gasLimit", "Limit of how much gas to spend")
+
+  .setAction(async (taskArgs) => {
+    // TODO
+  });
+
 // eslint-disable-next-line import/no-default-export
 export default config;
