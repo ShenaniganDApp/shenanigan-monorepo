035e412f28df233132594fb5343dee8a09f7854c
Ed Zynda <ezynda3@gmail.com>
2021-04-10 18:49:27 +0800

    remove buidler

--- packages/buidler/contracts/interfaces/ITokenManagement.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.0; 
-
-interface ITokenManagement {
-
-  function mint(address to, uint256 tokenId, string calldata challengeUrl, string calldata jsonUrl) external returns (uint256);
-  function fixFailedMessage(bytes32 _dataHash) external;
-
-}
