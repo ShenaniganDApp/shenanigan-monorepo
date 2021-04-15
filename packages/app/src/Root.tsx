27266c3a8d15426d76bc2e2d7767b3f472d14114
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-04-16 00:27:39 +0530

    chore: added relay to storybook

--- packages/app/src/Root.tsx
+++ packages/app/src/Root.tsx
@@ -1,15 +1,21 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/no-var-requires */
+import AsyncStorage from '@react-native-community/async-storage';
+import WalletConnectProvider from '@walletconnect/react-native-dapp';
 import React, { useEffect, useState } from 'react';
 import DevMenu from 'react-native-dev-menu';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RelayEnvironmentProvider } from 'react-relay';
 
 import Storybook from '../storybook';
-import { Providers } from './Providers';
+import { App } from './App';
+import { Web3ContextProvider } from './contexts';
+import Environment from './relay/Environment';
 
 const Root: React.FC = () => {
     const [storybookActive, setStorybookActive] = useState(false);
-    const toggleStorybook = () => setStorybookActive(active => !active);
+    const toggleStorybook = () => setStorybookActive((active) => !active);
 
     useEffect(() => {
         if (__DEV__) {
@@ -18,11 +24,27 @@ const Root: React.FC = () => {
         }
     }, []);
 
-    useEffect(() => {
-        console.log(storybookActive, __DEV__);
-    }, [storybookActive]);
-
-    return storybookActive ? <Storybook /> : <Providers />;
+    return (
+        <RelayEnvironmentProvider environment={Environment}>
+            <WalletConnectProvider
+                bridge="https://bridge.walletconnect.org"
+                clientMeta={{
+                    description: 'Shenanigan Developer App',
+                    url: 'https://she.energy',
+                    name: 'Shenanigan'
+                }}
+                storageOptions={{
+                    asyncStorage: AsyncStorage
+                }}
+            >
+                <Web3ContextProvider>
+                    <SafeAreaProvider>
+                        {storybookActive ? <Storybook /> : <App />}
+                    </SafeAreaProvider>
+                </Web3ContextProvider>
+            </WalletConnectProvider>
+        </RelayEnvironmentProvider>
+    );
 };
 
 export { Root };
--- packages/app/storybook/storyLoader.ts
+++ packages/app/storybook/storyLoader.ts
@@ -4,10 +4,14 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+    require('./stories/Comment/Comment.stories');
     require('./stories/Welcome/Welcome.stories');
 }
 
-const stories = ['./stories/Welcome/Welcome.stories'];
+const stories = [
+    './stories/Comment/Comment.stories',
+    './stories/Welcome/Welcome.stories'
+];
 
 module.exports = {
     loadStories,
