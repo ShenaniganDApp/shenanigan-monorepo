7d825835d4bb49cd2c8b1a9e986217a71bd655b5
VictorGinelli <ginelliunit@gmail.com>
2021-04-15 10:47:06 -0700

    Fix id equality checks

--- packages/server/src/graphql/modules/challenge/mutations/StartResultVote.ts
+++ packages/server/src/graphql/modules/challenge/mutations/StartResultVote.ts
@@ -1,66 +1,69 @@
-import { GraphQLInt, GraphQLNonNull, GraphQLString } from 'graphql';
-import { mutationWithClientMutationId } from 'graphql-relay';
-import { ChallengeLoader } from '../../../loaders';
-import { EVENTS, pubSub } from '../../../pubSub';
+import { GraphQLInt, GraphQLNonNull, GraphQLString } from "graphql";
+import { mutationWithClientMutationId } from "graphql-relay";
+import { ChallengeLoader } from "../../../loaders";
+import { EVENTS, pubSub } from "../../../pubSub";
 
-import { GraphQLContext } from '../../../TypeDefinition';
-import { errorField } from '../../../utils/errorField';
-import { successField } from '../../../utils/successField';
-import { ChallengeModel } from '../ChallengeModel';
-import { ChallengeType } from '../ChallengeType';
+import { GraphQLContext } from "../../../TypeDefinition";
+import { errorField } from "../../../utils/errorField";
+import { successField } from "../../../utils/successField";
+import { ChallengeModel } from "../ChallengeModel";
+import { ChallengeType } from "../ChallengeType";
 
 export const StartResultVote = mutationWithClientMutationId({
-	name: 'StartResultVote',
-	inputFields: {
-		challengeId: {
-			type: new GraphQLNonNull(GraphQLString),
-		},
-		blockTime: {
-			type: new GraphQLNonNull(GraphQLInt),
-		},
-	},
-	mutateAndGetPayload: async ({ challengeId, blockTime }, { user }: GraphQLContext) => {
-		if (!user) {
-			return {
-				error: 'User is not logged in',
-			};
-		}
-		const challenge = await ChallengeModel.findOne({ _id: challengeId });
-		if (!challenge) {
-			throw new Error('Challenge does not exist');
-		}
-		if (user._id !== challenge.creator) {
-			return {
-				error: 'User did not create the challenge',
-			};
-		}
-		if (challenge.active) {
-			return {
-				error: "Can't start a vote on an active challenge",
-			};
-		}
-		if (challenge.votePeriods.length > challenge.series) {
-			return {
-				error: 'A vote already exists for this challenge',
-			};
-		}
-		challenge.votePeriods.push([blockTime, blockTime + 86400]); //seconds in day
-		const result = await challenge.save();
-		await pubSub.publish(EVENTS.CHALLENGE.RESULT, {
-			challengeId: challenge._id,
-		});
-		return { id: result._id, error: null };
-	},
+  name: "StartResultVote",
+  inputFields: {
+    challengeId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    blockTime: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+  },
+  mutateAndGetPayload: async (
+    { challengeId, blockTime },
+    { user }: GraphQLContext
+  ) => {
+    if (!user) {
+      return {
+        error: "User is not logged in",
+      };
+    }
+    const challenge = await ChallengeModel.findOne({ _id: challengeId });
+    if (!challenge) {
+      throw new Error("Challenge does not exist");
+    }
+    if (user._id.toString() !== challenge.creator.toString()) {
+      return {
+        error: "User did not create the challenge",
+      };
+    }
+    if (challenge.active) {
+      return {
+        error: "Can't start a vote on an active challenge",
+      };
+    }
+    if (challenge.votePeriods.length > challenge.series) {
+      return {
+        error: "A vote already exists for this challenge",
+      };
+    }
+    challenge.votePeriods.push([blockTime, blockTime + 86400]); //seconds in day
+    const result = await challenge.save();
+    await pubSub.publish(EVENTS.CHALLENGE.RESULT, {
+      challengeId: challenge._id,
+    });
+    return { id: result._id, error: null };
+  },
 
-	outputFields: {
-		challenge: {
-			type: ChallengeType,
-			resolve: async ({ id }, _, context) => {
-				const challenge = await ChallengeLoader.load(context, id);
-				return challenge;
-			},
-		},
-		...errorField,
-		...successField,
-	},
+  outputFields: {
+    challenge: {
+      type: ChallengeType,
+      resolve: async ({ id }, _, context) => {
+        const challenge = await ChallengeLoader.load(context, id);
+        return challenge;
+      },
+    },
+    ...errorField,
+    ...successField,
+  },
 });
--- packages/server/src/graphql/modules/vote/mutations/CreateVote.ts
+++ packages/server/src/graphql/modules/vote/mutations/CreateVote.ts
@@ -1,132 +1,135 @@
-import { GraphQLID, GraphQLInt, GraphQLNonNull, GraphQLString } from 'graphql';
-import { EVENTS, pubSub } from '../../../pubSub';
-import { mutationWithClientMutationId, toGlobalId } from 'graphql-relay';
-import { VoteLoader } from '../../../loaders';
+import { GraphQLID, GraphQLInt, GraphQLNonNull, GraphQLString } from "graphql";
+import { EVENTS, pubSub } from "../../../pubSub";
+import { mutationWithClientMutationId, toGlobalId } from "graphql-relay";
+import { VoteLoader } from "../../../loaders";
 
-import { GraphQLContext } from '../../../TypeDefinition';
-import { ChallengeModel } from '../../challenge/ChallengeModel';
-import { VoteModel } from '../VoteModel';
-import { VoteConnection } from '../VoteType';
-import { errorField } from '../../../utils/errorField';
-import { successField } from '../../../utils/successField';
+import { GraphQLContext } from "../../../TypeDefinition";
+import { ChallengeModel } from "../../challenge/ChallengeModel";
+import { VoteModel } from "../VoteModel";
+import { VoteConnection } from "../VoteType";
+import { errorField } from "../../../utils/errorField";
+import { successField } from "../../../utils/successField";
 
-const voteTypes = ['OUTCOME', 'SKIP'];
+const voteTypes = ["OUTCOME", "SKIP"];
 
 type Args = {
-	choice: number;
-	voteType: string;
-	challengeId: string;
-	blockTime: number;
+  choice: number;
+  voteType: string;
+  challengeId: string;
+  blockTime: number;
 };
 export const CreateVote = mutationWithClientMutationId({
-	name: 'CreateVote',
-	inputFields: {
-		choice: {
-			type: new GraphQLNonNull(GraphQLInt),
-		},
-		voteType: {
-			type: new GraphQLNonNull(GraphQLString),
-		},
-		challengeId: {
-			type: new GraphQLNonNull(GraphQLString),
-		},
-		blockTime: {
-			type: new GraphQLNonNull(GraphQLInt),
-		},
-	},
-	mutateAndGetPayload: async ({ choice, challengeId, blockTime, voteType }: Args, { user }: GraphQLContext) => {
-		if (!user)
-			return {
-				error: 'Donation must be greater than 0',
-			};
-		if (voteTypes.indexOf(voteType) === -1)
-			return {
-				error: 'Vote must be of a valid voting type',
-			};
+  name: "CreateVote",
+  inputFields: {
+    choice: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+    voteType: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    challengeId: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    blockTime: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+  },
+  mutateAndGetPayload: async (
+    { choice, challengeId, blockTime, voteType }: Args,
+    { user }: GraphQLContext
+  ) => {
+    if (!user)
+      return {
+        error: "Donation must be greater than 0",
+      };
+    if (voteTypes.indexOf(voteType) === -1)
+      return {
+        error: "Vote must be of a valid voting type",
+      };
 
-		const fetchedChallenge = await ChallengeModel.findById(challengeId);
-		if (!fetchedChallenge)
-			return {
-				error: 'Challenge not found.',
-			};
+    const fetchedChallenge = await ChallengeModel.findById(challengeId);
+    if (!fetchedChallenge)
+      return {
+        error: "Challenge not found.",
+      };
 
-		const voteExists = await VoteModel.findOne({
-			challenge: challengeId,
-			creator: user._id,
-			challengeSeries: fetchedChallenge.series,
-			voteType,
-		});
-		if (voteExists)
-			return {
-				error: 'Vote already exists for this user',
-			};
-		if (user._id === fetchedChallenge.creator)
-			return {
-				error: 'User cannot vote on a challenge they own',
-			};
+    const voteExists = await VoteModel.findOne({
+      challenge: challengeId,
+      creator: user._id,
+      challengeSeries: fetchedChallenge.series,
+      voteType,
+    });
+    if (voteExists)
+      return {
+        error: "Vote already exists for this user",
+      };
+    if (user._id.toString() === fetchedChallenge.creator.toString())
+      return {
+        error: "User cannot vote on a challenge they own",
+      };
 
-		const vote = new VoteModel({
-			creator: user._id,
-			choice,
-			challenge: fetchedChallenge,
-			challengeSeries: fetchedChallenge.series,
-			voteType,
-		});
+    const vote = new VoteModel({
+      creator: user._id,
+      choice,
+      challenge: fetchedChallenge,
+      challengeSeries: fetchedChallenge.series,
+      voteType,
+    });
 
-		if (voteType === 'OUTCOME') {
-			if (fetchedChallenge.active || fetchedChallenge.live)
-				return {
-					error: 'Challenge cannot be active or live to outcome vote',
-				};
-			const voteEnd = fetchedChallenge.votePeriods[fetchedChallenge.series][1];
-			if (blockTime > voteEnd)
-				return {
-					error: 'Vote is already closed',
-				};
-			fetchedChallenge.outcomeVotes.push(vote._id);
-			user.outcomeVotes.push(vote._id);
-		}
+    if (voteType === "OUTCOME") {
+      if (fetchedChallenge.active || fetchedChallenge.live)
+        return {
+          error: "Challenge cannot be active or live to outcome vote",
+        };
+      const voteEnd = fetchedChallenge.votePeriods[fetchedChallenge.series][1];
+      if (blockTime > voteEnd)
+        return {
+          error: "Vote is already closed",
+        };
+      fetchedChallenge.outcomeVotes.push(vote._id);
+      user.outcomeVotes.push(vote._id);
+    }
 
-		if (voteType === 'SKIP') {
-			if (choice > 1)
-				return {
-					error: 'Skip vote choice must be a 0 or 1',
-				};
-			if (!fetchedChallenge.live)
-				return {
-					error: 'Challenge must be live to skip vote',
-				};
-			fetchedChallenge.skipVotes.push(vote._id);
-			user.skipVotes.push(vote._id);
-		}
+    if (voteType === "SKIP") {
+      if (choice > 1)
+        return {
+          error: "Skip vote choice must be a 0 or 1",
+        };
+      if (!fetchedChallenge.live)
+        return {
+          error: "Challenge must be live to skip vote",
+        };
+      fetchedChallenge.skipVotes.push(vote._id);
+      user.skipVotes.push(vote._id);
+    }
 
-		await vote.save();
+    await vote.save();
 
-		await fetchedChallenge.save();
-		await user.save();
-		await pubSub.publish(EVENTS.VOTE.ADDED, { voteId: vote._id });
+    await fetchedChallenge.save();
+    await user.save();
+    await pubSub.publish(EVENTS.VOTE.ADDED, { voteId: vote._id });
 
-		return vote;
-	},
-	outputFields: {
-		voteEdge: {
-			type: VoteConnection.edgeType,
-			resolve: async ({ id }, _, context) => {
-				// Load new edge from loader
-				const vote = await VoteLoader.load(context, id);
+    return vote;
+  },
+  outputFields: {
+    voteEdge: {
+      type: VoteConnection.edgeType,
+      resolve: async ({ id }, _, context) => {
+        // Load new edge from loader
+        const vote = await VoteLoader.load(context, id);
 
-				// Returns null if no node was loaded
-				if (!vote) {
-					return null;
-				}
+        // Returns null if no node was loaded
+        if (!vote) {
+          return null;
+        }
 
-				return {
-					cursor: toGlobalId('Vote', vote._id),
-					node: vote,
-				};
-			},
-		},
-		...successField,
-		...errorField,
-	},
+        return {
+          cursor: toGlobalId("Vote", vote._id),
+          node: vote,
+        };
+      },
+    },
+    ...successField,
+    ...errorField,
+  },
 });
