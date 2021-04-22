2646bdebf85de6dc43d955f813a1309861e48403
Tawnee <allprt89@gmail.com>
2021-04-22 12:38:09 -0600

    add notch component. adjust gradient

--- packages/app/src/components/UI/Gradient.tsx
+++ packages/app/src/components/UI/Gradient.tsx
@@ -10,11 +10,16 @@ type Props = {
 const Gradient = ({ children, style }: Props): ReactElement => {
     return (
         <LinearGradient
-            colors={['#03B3FF', '#38234A', '#DF2D71', '#FB7429']}
-            locations={[0.09, 0.25, 0.65, 1]}
+            colors={[
+                '#03B3FF',
+                'rgba(80, 62, 151, 0.85)',
+                '#rgba(223, 45, 113, 0.9)',
+                '#FB7429'
+            ]}
+            locations={[0.05, 0.27, 0.56, 1]}
             style={[{ flex: 1 }, style]}
             useAngle
-            angle={202}
+            angle={220}
         >
             {children}
         </LinearGradient>
