0b5c50a888202e2e0c68ce661f993a5058b5ef2c
Tawnee <allprt89@gmail.com>
2021-05-02 14:58:42 -0600

    add gradient easing

--- packages/app/src/components/UI/Gradient.tsx
+++ packages/app/src/components/UI/Gradient.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, ReactNode } from 'react';
-import { ViewStyle } from 'react-native';
+import { View, ViewStyle } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import easeGradient from 'react-native-easing-gradient';
 
 type Props = {
     children: ReactNode | ReactNode[];
@@ -8,21 +9,35 @@ type Props = {
 };
 
 const Gradient = ({ children, style }: Props): ReactElement => {
+    const { colors, locations } = easeGradient({
+        colorStops: {
+            0.02: {
+                color: '#03B3FF'
+            },
+            0.27: {
+                color: 'rgba(80, 62, 151, 0.85)'
+            },
+            0.56: {
+                color: 'rgba(223, 45, 113, 0.9)'
+            },
+            1: {
+                color: '#FB7429'
+            }
+        }
+    });
+
     return (
-        <LinearGradient
-            colors={[
-                '#03B3FF',
-                'rgba(80, 62, 151, 0.85)',
-                '#rgba(223, 45, 113, 0.9)',
-                '#FB7429'
-            ]}
-            locations={[0.05, 0.27, 0.56, 1]}
-            style={[{ flex: 1 }, style]}
-            useAngle
-            angle={220}
-        >
-            {children}
-        </LinearGradient>
+        <View style={[{ backgroundColor: 'white', flex: 1 }, style]}>
+            <LinearGradient
+                colors={colors}
+                locations={locations}
+                style={[{ flex: 1 }, style]}
+                useAngle
+                angle={220}
+            >
+                {children}
+            </LinearGradient>
+        </View>
     );
 };
 
