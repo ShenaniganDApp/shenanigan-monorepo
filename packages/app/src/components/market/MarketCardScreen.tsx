4ef614a7b6cc0334dfffc822de3f72eb2f128736
Tawnee <allprt89@gmail.com>
2021-06-23 12:26:08 -0600

    add styles to card screen. add tag component.

--- packages/app/src/components/market/MarketCardScreen.tsx
+++ packages/app/src/components/market/MarketCardScreen.tsx
@@ -1,6 +1,19 @@
 import React, { ReactElement, useContext, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { TabNavSwipeContext } from '../../contexts';
+import {
+    Button,
+    colors,
+    Gradient,
+    ImageCard,
+    Notch,
+    sizes,
+    Tag,
+    Title,
+    XdaiBanner
+} from '../UI';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type Props = {};
 
@@ -9,13 +22,125 @@ export const MarketCardScreen = (props: Props): ReactElement => {
 
     useEffect(() => setMainTabsSwipe(false), []);
 
+    const tags = ['xDai', 'sportsball', 'other'];
+
     return (
-        <View style={styles.container}>
-            <Text>CardScreen</Text>
-        </View>
+        <Gradient>
+            <SafeAreaView>
+                <View style={styles.container}>
+                    <Icon
+                        name="arrow-left-bold-circle"
+                        size={38}
+                        color={colors.pink}
+                        style={styles.icon}
+                    />
+                    <View style={styles.imageContainer}>
+                        <ImageCard
+                            height={sizes.smallScreen ? 200 : 300}
+                            source={{
+                                uri:
+                                    'https://images.unsplash.com/photo-1474224017046-182ece80b263?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80'
+                            }}
+                        />
+                    </View>
+                    <View style={styles.textContainer}>
+                        <Title size={30} style={styles.title}>
+                            Watch Me Lift 1,000 lbs
+                        </Title>
+                        <Title size={19} style={styles.subtitle}>
+                            by YoungKidWarrior
+                        </Title>
+                        <Notch
+                            title="I whiff completely"
+                            style={{ alignSelf: 'flex-start' }}
+                        />
+                        <Text style={styles.body}>
+                            This is a description of the card. It might be the
+                            challenge description the athlete inputs, or maybe
+                            it’s made when the owner puts it up for sale.
+                        </Text>
+                        <View style={styles.tagContainer}>
+                            {tags.map((tag) => (
+                                <Tag text={tag} key={tag} style={styles.tag} />
+                            ))}
+                        </View>
+                        <Text style={styles.currentPrice}>Current Price</Text>
+                        <View style={styles.availabilityContainer}>
+                            <XdaiBanner
+                                amount="99,999"
+                                style={{ alignSelf: 'flex-start' }}
+                            />
+                            <Title size={19}>100/420 Available</Title>
+                        </View>
+                        <Button
+                            title="Buy Now"
+                            fullWidth
+                            onPress={() => console.log('press')}
+                        />
+                        <Text style={styles.more}>
+                            25 more available from 999,999
+                        </Text>
+                    </View>
+                </View>
+            </SafeAreaView>
+        </Gradient>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {}
+    container: {
+        paddingHorizontal: '4%'
+    },
+    icon: {
+        textShadowColor: 'rgba(0, 0, 0, 0.3)',
+        textShadowOffset: { width: 0, height: 4 },
+        textShadowRadius: 5,
+        marginLeft: '4%',
+        paddingBottom: '2%'
+    },
+    imageContainer: {
+        alignItems: 'center',
+        marginBottom: '4%'
+    },
+    textContainer: {
+        paddingHorizontal: '4%',
+        paddingVertical: '4%',
+        borderColor: 'rgba(251, 250, 250, 0.7)',
+        borderWidth: 1,
+        borderRadius: 10,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)'
+    },
+    title: {
+        marginBottom: 2
+    },
+    subtitle: {
+        marginBottom: '2%'
+    },
+    body: {
+        marginVertical: '2%'
+    },
+    tagContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginVertical: '2%',
+        flexWrap: 'wrap'
+    },
+    tag: {
+        marginHorizontal: '2%',
+        marginBottom: '2%'
+    },
+    currentPrice: {
+        color: colors.gray
+    },
+    availabilityContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: '4%'
+    },
+    more: {
+        textAlign: 'center',
+        marginTop: '4%',
+        fontSize: 16,
+        textDecorationLine: 'underline'
+    }
 });
