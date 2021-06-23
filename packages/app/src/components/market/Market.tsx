b2c21e6ed56452b45195934b6f0a99e50a979141
Tawnee <allprt89@gmail.com>
2021-06-23 11:17:55 -0600

    add tabswipe context

--- packages/app/src/components/market/Market.tsx
+++ packages/app/src/components/market/Market.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useContext, useEffect, ReactElement } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useQuery, graphql } from 'relay-hooks';
@@ -7,6 +7,8 @@ import { MarketList } from './MarketList';
 import { SearchBar } from './SearchBar';
 import { Filters } from './Filters';
 import { MarketQuery } from './__generated__/MarketQuery.graphql';
+import { TabNavSwipeContext } from '../../contexts';
+import { useNavigation } from '@react-navigation/native';
 
 const query = graphql`
     query MarketQuery {
@@ -14,8 +16,17 @@ const query = graphql`
     }
 `;
 
-export const Market = (): React.ReactElement => {
+export const Market = (): ReactElement => {
     const { props: data, retry } = useQuery<MarketQuery>(query);
+    const { setMainTabsSwipe } = useContext(TabNavSwipeContext);
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const focus = navigation.addListener('focus', () => {
+            setMainTabsSwipe(true);
+        });
+        return focus;
+    }, [navigation]);
 
     return (
         <Gradient>
