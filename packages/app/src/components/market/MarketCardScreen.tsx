68a861a5b958c2f872d518fb2f62af043c9dd6f4
Tawnee <allprt89@gmail.com>
2021-06-23 12:57:46 -0600

    remove safeareaview. hook up back button

--- packages/app/src/components/market/MarketCardScreen.tsx
+++ packages/app/src/components/market/MarketCardScreen.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement, useContext, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { TabNavSwipeContext } from '../../contexts';
 import {
     Button,
@@ -12,13 +13,14 @@ import {
     Title,
     XdaiBanner
 } from '../UI';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 type Props = {};
 
 export const MarketCardScreen = (props: Props): ReactElement => {
     const { setMainTabsSwipe } = useContext(TabNavSwipeContext);
+    const navigation = useNavigation();
 
     useEffect(() => setMainTabsSwipe(false), []);
 
@@ -26,76 +28,80 @@ export const MarketCardScreen = (props: Props): ReactElement => {
 
     return (
         <Gradient>
-            <SafeAreaView>
-                <View style={styles.container}>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
                     <Icon
-                        name="arrow-left-bold-circle"
-                        size={38}
+                        name="chevron-left"
+                        size={52}
                         color={colors.pink}
                         style={styles.icon}
                     />
-                    <View style={styles.imageContainer}>
-                        <ImageCard
-                            height={sizes.smallScreen ? 200 : 300}
-                            source={{
-                                uri:
-                                    'https://images.unsplash.com/photo-1474224017046-182ece80b263?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80'
-                            }}
-                        />
+                </TouchableOpacity>
+                <View style={styles.imageContainer}>
+                    <ImageCard
+                        height={sizes.smallScreen ? 200 : 300}
+                        source={{
+                            uri:
+                                'https://images.unsplash.com/photo-1474224017046-182ece80b263?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80'
+                        }}
+                    />
+                </View>
+                <View style={styles.textContainer}>
+                    <Title size={30} style={styles.title}>
+                        Watch Me Lift 1,000 lbs
+                    </Title>
+
+                    <Title size={19} style={styles.subtitle}>
+                        by YoungKidWarrior
+                    </Title>
+
+                    <Notch
+                        title="I whiff completely"
+                        style={{ alignSelf: 'flex-start' }}
+                    />
+                    <Text style={styles.body}>
+                        This is a description of the card. It might be the
+                        challenge description the athlete inputs, or maybe it’s
+                        made when the owner puts it up for sale.
+                    </Text>
+
+                    <View style={styles.tagContainer}>
+                        {tags.map((tag) => (
+                            <Tag text={tag} key={tag} style={styles.tag} />
+                        ))}
                     </View>
-                    <View style={styles.textContainer}>
-                        <Title size={30} style={styles.title}>
-                            Watch Me Lift 1,000 lbs
-                        </Title>
-                        <Title size={19} style={styles.subtitle}>
-                            by YoungKidWarrior
-                        </Title>
-                        <Notch
-                            title="I whiff completely"
+
+                    <Text style={styles.currentPrice}>Current Price</Text>
+                    <View style={styles.availabilityContainer}>
+                        <XdaiBanner
+                            amount="99,999"
                             style={{ alignSelf: 'flex-start' }}
                         />
-                        <Text style={styles.body}>
-                            This is a description of the card. It might be the
-                            challenge description the athlete inputs, or maybe
-                            it’s made when the owner puts it up for sale.
-                        </Text>
-                        <View style={styles.tagContainer}>
-                            {tags.map((tag) => (
-                                <Tag text={tag} key={tag} style={styles.tag} />
-                            ))}
-                        </View>
-                        <Text style={styles.currentPrice}>Current Price</Text>
-                        <View style={styles.availabilityContainer}>
-                            <XdaiBanner
-                                amount="99,999"
-                                style={{ alignSelf: 'flex-start' }}
-                            />
-                            <Title size={19}>100/420 Available</Title>
-                        </View>
-                        <Button
-                            title="Buy Now"
-                            fullWidth
-                            onPress={() => console.log('press')}
-                        />
-                        <Text style={styles.more}>
-                            25 more available from 999,999
-                        </Text>
+                        <Title size={19}>100/420 Available</Title>
                     </View>
+                    <Button
+                        title="Buy Now"
+                        fullWidth
+                        onPress={() => console.log('press')}
+                    />
+                    <Text style={styles.more}>
+                        25 more available from 999,999
+                    </Text>
                 </View>
-            </SafeAreaView>
+            </View>
         </Gradient>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        paddingHorizontal: '4%'
+        paddingHorizontal: '4%',
+        paddingTop: '10%'
     },
     icon: {
         textShadowColor: 'rgba(0, 0, 0, 0.3)',
         textShadowOffset: { width: 0, height: 4 },
         textShadowRadius: 5,
-        marginLeft: '4%',
         paddingBottom: '2%'
     },
     imageContainer: {
