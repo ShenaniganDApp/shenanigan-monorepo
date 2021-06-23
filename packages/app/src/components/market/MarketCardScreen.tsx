d40b7f59485b4a0376a8f208896835ee2b5bfb3b
Tawnee <allprt89@gmail.com>
2021-06-23 11:17:55 -0600

    add tabswipe context

--- packages/app/src/components/market/MarketCardScreen.tsx
+++ packages/app/src/components/market/MarketCardScreen.tsx
@@ -1,9 +1,14 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useContext, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { TabNavSwipeContext } from '../../contexts';
 
 type Props = {};
 
 export const MarketCardScreen = (props: Props): ReactElement => {
+    const { setMainTabsSwipe } = useContext(TabNavSwipeContext);
+
+    useEffect(() => setMainTabsSwipe(false), []);
+
     return (
         <View style={styles.container}>
             <Text>CardScreen</Text>
