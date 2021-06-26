543bd85119aec8085a7d7738cb89bb6906dfd1e8
Tawnee <allprt89@gmail.com>
2021-06-26 17:38:03 -0600

    add responsive styling to xdaibanner and cards

--- packages/app/src/components/market/MarketCard.tsx
+++ packages/app/src/components/market/MarketCard.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { ImageCard, XdaiBanner } from '../UI';
+import { ImageCard, sizes, XdaiBanner } from '../UI';
 type Props = {};
 
 export const MarketCard = (props: Props): ReactElement => {
@@ -13,14 +13,14 @@ export const MarketCard = (props: Props): ReactElement => {
             >
                 <View style={styles.cardWrapper}>
                     <ImageCard
-                        height={150}
+                        height={sizes.smallScreen ? 140 : 150}
                         source={{
                             uri:
                                 'https://images.unsplash.com/photo-1474224017046-182ece80b263?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80'
                         }}
                     />
                     <Image
-                        style={[styles.profileImage, { height: 20, width: 20 }]}
+                        style={styles.profileImage}
                         height={25}
                         width={25}
                         resizeMode="cover"
@@ -50,6 +50,8 @@ const styles = StyleSheet.create({
         marginBottom: 6
     },
     profileImage: {
+        height: 25,
+        width: 25,
         borderRadius: 15,
         position: 'absolute',
         bottom: 5,
