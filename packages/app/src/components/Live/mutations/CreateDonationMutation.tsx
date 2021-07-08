98152f72ed1842d780d39544cae8649e7a7edd73
Prasad Walvekar <prasadwalvekar@gmail.com>
2021-07-08 10:55:22 +0530

    Hook up donate button with create mutation action

---
+++ packages/app/src/components/Live/mutations/CreateDonationMutation.tsx
@@ -0,0 +1,14 @@
+import { graphql } from 'react-relay';
+
+export const CreateDonation = graphql`
+    mutation CreateDonationMutation($input: CreateDonationInput!) {
+        CreateDonation(input: $input) {
+            error
+            donationEdge {
+                node {
+                    id
+                }
+            }
+        }
+    }
+`;
