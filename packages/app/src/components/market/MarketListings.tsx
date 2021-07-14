08b118776a7ecd7c8ec09c5ececd0f62ba07e6b7
Tawnee <allprt89@gmail.com>
2021-07-13 18:55:44 -0600

    add card and list of cards

--- packages/app/src/components/market/MarketListings.tsx
+++ packages/app/src/components/market/MarketListings.tsx
@@ -1,16 +1,23 @@
-import React, { ReactElement, useContext } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { ReactElement } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import { BlurView } from '@react-native-community/blur';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { TouchableOpacity, FlatList } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { colors, Title } from '../UI';
+import { Button, Card, colors, Title, XdaiBanner } from '../UI';
 
 type Props = {
     setListingsVisible: (b: boolean) => void;
 };
 
+/*
+    6. fade animation
+*/
+
 export const MarketListings = ({ setListingsVisible }: Props): ReactElement => {
+    const insets = useSafeAreaInsets();
+    const test = Array.from(Array(20).keys());
+
     return (
         <View style={[StyleSheet.absoluteFill, styles.flex]}>
             <BlurView
@@ -19,42 +26,78 @@ export const MarketListings = ({ setListingsVisible }: Props): ReactElement => {
                 blurAmount={4}
                 reducedTransparencyFallbackColor="rgba(255,255,255,.5)"
             />
-            <SafeAreaView style={styles.flex}>
-                <View style={styles.container}>
-                    <View style={styles.headerContainer}>
-                        <View style={styles.iconContainer}>
-                            <TouchableOpacity
-                                onPress={() => setListingsVisible(false)}
-                            >
-                                <Icon
-                                    name="chevron-left"
-                                    size={52}
-                                    color={colors.pink}
-                                    style={styles.icon}
-                                />
-                            </TouchableOpacity>
-                        </View>
-                        <Title style={styles.title} shadow>
-                            Other Sellers
-                        </Title>
+            <View style={[styles.flex, { paddingTop: insets.top }]}>
+                <View>
+                    <View style={styles.iconContainer}>
+                        <TouchableOpacity
+                            onPress={() => setListingsVisible(false)}
+                        >
+                            <Icon
+                                name="chevron-left"
+                                size={52}
+                                color={colors.pink}
+                                style={styles.icon}
+                            />
+                        </TouchableOpacity>
                     </View>
+                    <Title style={styles.title} shadow>
+                        Other Sellers
+                    </Title>
                 </View>
-            </SafeAreaView>
+
+                <FlatList
+                    data={test}
+                    style={styles.list}
+                    contentContainerStyle={styles.contentContainer}
+                    renderItem={({ item }) => {
+                        return (
+                            <Card style={styles.card}>
+                                <Title
+                                    size={18}
+                                    style={[styles.grayColor, styles.cardText]}
+                                >
+                                    from prettyscone shans
+                                </Title>
+                                <Text
+                                    style={[styles.grayColor, styles.cardText]}
+                                >
+                                    Current price
+                                </Text>
+                                <View style={styles.row}>
+                                    <XdaiBanner amount="99,999" />
+                                    <Title size={18} style={styles.grayColor}>
+                                        15/15 Available
+                                    </Title>
+                                </View>
+                                <Button
+                                    title="Buy Now"
+                                    fullWidth
+                                    style={styles.cardButton}
+                                />
+                            </Card>
+                        );
+                    }}
+                    // keyExtractor={(item) => item.node._id}
+                />
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    grayColor: {
+        color: colors.gray
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
     flex: {
         flex: 1
     },
-    container: {
-        paddingHorizontal: '4%'
-    },
-    headerContainer: {},
     iconContainer: {
         position: 'absolute',
-        left: 0,
+        left: '4%',
         top: 0,
         zIndex: 1
     },
@@ -65,5 +108,22 @@ const styles = StyleSheet.create({
     },
     title: {
         textAlign: 'center'
+    },
+    list: {
+        marginTop: '4%'
+    },
+    contentContainer: {
+        paddingHorizontal: '4%',
+        paddingBottom: '4%'
+    },
+    card: {
+        backgroundColor: 'rgba(255,255,255,.75)',
+        marginBottom: '4%'
+    },
+    cardText: {
+        marginBottom: '1%'
+    },
+    cardButton: {
+        marginTop: '3%'
     }
 });
