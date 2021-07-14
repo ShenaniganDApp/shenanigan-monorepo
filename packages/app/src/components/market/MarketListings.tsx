326cfbc653737ad728d5e3eace3c5f78888e848b
Tawnee <allprt89@gmail.com>
2021-07-13 18:26:44 -0600

    add MarketListing modal

--- packages/app/src/components/market/MarketListings.tsx
+++ packages/app/src/components/market/MarketListings.tsx
@@ -1,19 +1,69 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useContext } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { Gradient } from '../UI';
+import { BlurView } from '@react-native-community/blur';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { colors, Title } from '../UI';
 
-type Props = {};
+type Props = {
+    setListingsVisible: (b: boolean) => void;
+};
 
-export const MarketListings = (props: Props): ReactElement => {
+export const MarketListings = ({ setListingsVisible }: Props): ReactElement => {
     return (
-        <Gradient>
-            <View style={styles.container}>
-                <Text>component</Text>
-            </View>
-        </Gradient>
+        <View style={[StyleSheet.absoluteFill, styles.flex]}>
+            <BlurView
+                style={StyleSheet.absoluteFill}
+                blurType="light"
+                blurAmount={4}
+                reducedTransparencyFallbackColor="rgba(255,255,255,.5)"
+            />
+            <SafeAreaView style={styles.flex}>
+                <View style={styles.container}>
+                    <View style={styles.headerContainer}>
+                        <View style={styles.iconContainer}>
+                            <TouchableOpacity
+                                onPress={() => setListingsVisible(false)}
+                            >
+                                <Icon
+                                    name="chevron-left"
+                                    size={52}
+                                    color={colors.pink}
+                                    style={styles.icon}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <Title style={styles.title} shadow>
+                            Other Sellers
+                        </Title>
+                    </View>
+                </View>
+            </SafeAreaView>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {}
+    flex: {
+        flex: 1
+    },
+    container: {
+        paddingHorizontal: '4%'
+    },
+    headerContainer: {},
+    iconContainer: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        zIndex: 1
+    },
+    icon: {
+        textShadowColor: 'rgba(0, 0, 0, 0.3)',
+        textShadowOffset: { width: 0, height: 4 },
+        textShadowRadius: 5
+    },
+    title: {
+        textAlign: 'center'
+    }
 });
