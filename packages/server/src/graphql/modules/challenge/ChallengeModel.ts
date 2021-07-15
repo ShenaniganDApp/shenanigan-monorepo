369c64cced9a0b0d58c8d500bab670e3c5514f52
VictorGinelli <ginelliunit@gmail.com>
2021-07-15 15:43:00 -0700

    Use a string for currency amounts Fixes #176

--- packages/server/src/graphql/modules/challenge/ChallengeModel.ts
+++ packages/server/src/graphql/modules/challenge/ChallengeModel.ts
@@ -1,121 +1,125 @@
-import mongoose, { Document, Model, Types } from 'mongoose';
+import mongoose, { Document, Model, Types } from "mongoose";
 
 const { Schema } = mongoose;
 
 const challengeSchema = new Schema(
-	{
-		address: {
-			type: String,
-			required: true,
-		},
-		title: {
-			type: String,
-			required: true,
-		},
-		content: {
-			type: String,
-		},
-		series: {
-			type: Number,
-			required: true,
-			default: 0,
-		},
-		active: {
-			type: Boolean,
-			default: true,
-			required: true,
-		},
-		live: {
-			type: Boolean,
-			default: false,
-			required: true,
-		},
-		color: {
-			type: String,
-			default: `hsl(${360 * Math.random()}, 100%, 55%)`,
-			required: true,
-		},
-		votePeriods: [
-			[
-				{
-					type: Number,
-				},
-			],
-		],
-		image: {
-			type: String,
-			required: true,
-		},
-		positiveOptions: [
-			{
-				type: String,
-				required: true,
-			},
-		],
-		negativeOptions: [
-			{
-				type: String,
-				required: true,
-			},
-		],
-		donations: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: 'Donation',
-			},
-		],
-		totalDonations: {
-			type: Number,
-			required: true,
-			default: 0,
-		},
-		outcomeVotes: [{ type: Schema.Types.ObjectId, ref: 'Vote' }],
-		skipVotes: [{ type: Schema.Types.ObjectId, ref: 'Vote' }],
-		creator: {
-			type: Schema.Types.ObjectId,
-			ref: 'User',
-			required: true,
-		},
-		comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-		predictions: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: 'Prediction',
-			},
-		],
-		challengeCards: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: 'ChallengeCard',
-			},
-		],
-	},
-	{ timestamps: true, collection: 'challenges' }
+  {
+    address: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+    },
+    series: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+    live: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: `hsl(${360 * Math.random()}, 100%, 55%)`,
+      required: true,
+    },
+    votePeriods: [
+      [
+        {
+          type: Number,
+        },
+      ],
+    ],
+    image: {
+      type: String,
+      required: true,
+    },
+    positiveOptions: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    negativeOptions: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    donations: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Donation",
+      },
+    ],
+    totalDonations: {
+      type: String,
+      required: true,
+      default: "0",
+    },
+    outcomeVotes: [{ type: Schema.Types.ObjectId, ref: "Vote" }],
+    skipVotes: [{ type: Schema.Types.ObjectId, ref: "Vote" }],
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    predictions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Prediction",
+      },
+    ],
+    challengeCards: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "ChallengeCard",
+      },
+    ],
+  },
+  { timestamps: true, collection: "challenges" }
 );
 
 export interface IChallenge extends Document {
-	address: string;
-	title: string;
-	content?: string;
-	series: number;
-	active: boolean;
-	live: boolean;
-	image: string;
-	positiveOptions: string[];
-	negativeOptions: string[];
-	color: string;
-	donations: Types.ObjectId[];
-	predictions: Types.ObjectId[];
-	creator: Types.ObjectId;
-	comments: Types.ObjectId[];
-	challengeCards: Types.ObjectId[];
-	votePeriods: number[][];
-	outcomeVotes: Types.ObjectId[];
-	skipVotes: Types.ObjectId[];
-	createdAt: Date;
-	updatedAt: Date;
+  address: string;
+  title: string;
+  content?: string;
+  series: number;
+  active: boolean;
+  live: boolean;
+  image: string;
+  positiveOptions: string[];
+  negativeOptions: string[];
+  color: string;
+  totalDonations: string;
+  donations: Types.ObjectId[];
+  predictions: Types.ObjectId[];
+  creator: Types.ObjectId;
+  comments: Types.ObjectId[];
+  challengeCards: Types.ObjectId[];
+  votePeriods: number[][];
+  outcomeVotes: Types.ObjectId[];
+  skipVotes: Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const ChallengeModel: Model<IChallenge> = mongoose.model('Challenge', challengeSchema);
+const ChallengeModel: Model<IChallenge> = mongoose.model(
+  "Challenge",
+  challengeSchema
+);
 
 export { ChallengeModel };
