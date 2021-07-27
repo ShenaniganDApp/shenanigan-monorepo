2ef4e84b5b06710174dea810e1f70476ad929cab
Tawnee <allprt89@gmail.com>
2021-07-26 18:06:51 -0600

    fix types. add keyboard handling and scrollviews

--- packages/app/src/components/challenges/Outcomes.tsx
+++ packages/app/src/components/challenges/Outcomes.tsx
@@ -1,22 +1,26 @@
 import React, { ReactElement } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, StyleSheet, KeyboardAvoidingView } from 'react-native';
 import { Title } from '../UI';
 import { FormType } from './CreateChallengeScreen';
 import { OutcomeCard } from './OutcomeCard';
 
 type Props = {
     form: FormType;
-    setForm: (fn: (f: FormType) => void) => void;
+    setForm: (form: FormType | ((prevState: FormType) => FormType)) => void;
 };
 
 /*
- * keyboard avoiding view
- * remove form placeholders
- */
+
+when list is scrolled to bottom and one above is deleted, move the list down
+
+*/
 
 export const Outcomes = ({ form, setForm }: Props): ReactElement => {
     return (
-        <View>
+        <KeyboardAvoidingView
+            behavior={'position'}
+            keyboardVerticalOffset={120}
+        >
             <Title style={styles.title}>Outcomes</Title>
             <Text style={styles.description}>
                 Describe what success and failure look like on this challenge.
@@ -35,7 +39,7 @@ export const Outcomes = ({ form, setForm }: Props): ReactElement => {
                 form={form}
                 setForm={setForm}
             />
-        </View>
+        </KeyboardAvoidingView>
     );
 };
 
