256a61fad3550f356f721556608474fc508c6351
VictorGinelli <ginelliunit@gmail.com>
2021-07-26 23:11:20 -0700

    GIve helpful tip about tokens

--- packages/app/src/relay/fetchQuery.tsx
+++ packages/app/src/relay/fetchQuery.tsx
@@ -13,9 +13,12 @@ const fetchQuery = async (
     uploadables: UploadableMap
 ): Promise<any> => {
     try {
-        // const token = await AsyncStorage.getItem('token');
-        const token =
-            'WyIweGRhNzNlYTlmN2E2NzE4ZDFkZjU4YWExNDAwNjM4ZWUzYTEzYzU0ZTJkMjU1MmZlNjM5ODc2ZGI3NTA5MGE5MDE1ODc4YWNiNDQwMjg3YTU3Y2EyOGM0ZWQwNTgwNzc2ZDBjM2ViOTU5NTQwYjc0OWZkMmRhMTNiMTc1MWUzOWE0MWMiLCJ7XCJpYXRcIjoxNjI2MjM4ODQxNjIyLFwiZXhwXCI6MTYyNjg0MzY0MTYyMixcImlzc1wiOlwiMHhiMTkzRTI3NTM0OEZlNGEzRTUzNjgyNzI4MDg2YjE0OTVDRDAyYzZEXCIsXCJhdWRcIjpcInNoZW5hbmlnYW5cIixcInRpZFwiOlwiOWMwNmY1OTMtNWE5Ny00ZDZjLWJkYWUtYTM2ZDRkYTc2YWIwXCJ9Il0=';
+        //If you are having trouble opening the app, comment this line and uncomment the one below it. Open the app then undo the changes here
+        const token = await AsyncStorage.getItem('token');
+        // const token =
+        //     'WyIweGRhNzNlYTlmN2E2NzE4ZDFkZjU4YWExNDAwNjM4ZWUzYTEzYzU0ZTJkMjU1MmZlNjM5ODc2ZGI3NTA5MGE5MDE1ODc4YWNiNDQwMjg3YTU3Y2EyOGM0ZWQwNTgwNzc2ZDBjM2ViOTU5NTQwYjc0OWZkMmRhMTNiMTc1MWUzOWE0MWMiLCJ7XCJpYXRcIjoxNjI2MjM4ODQxNjIyLFwiZXhwXCI6MTYyNjg0MzY0MTYyMixcImlzc1wiOlwiMHhiMTkzRTI3NTM0OEZlNGEzRTUzNjgyNzI4MDg2YjE0OTVDRDAyYzZEXCIsXCJhdWRcIjpcInNoZW5hbmlnYW5cIixcInRpZFwiOlwiOWMwNmY1OTMtNWE5Ny00ZDZjLWJkYWUtYTM2ZDRkYTc2YWIwXCJ9Il0=';
+        //
+        // challengeOwner = WyIweDM4YzRkNjg3MzQwNzE1NjdlNDBjZjkwMjRiZTA4ODA1MzRmOTQ4NzBiNTQ5OGUzMjVjN2MyOTBmMzBhMTU5OWQ0ODc1YWYyOTcwYWNhOWFmMWFmMTI0NWQzMTZjZTk2NmZkOWFhYTkxZjE1NGYzZjI0ODQyYWEzM2U0YmYzMmJiMWIiLCJ7XCJpYXRcIjoxNjI3MzY1MTgwNjkwLFwiZXhwXCI6MTYyNzk2OTk4MDY5MCxcImlzc1wiOlwiMHgxNjg2NjUxZDI1ZWE0OTQ3ZjBCREI1ODJFQjIzZDU5OWY4NmEzMjM0XCIsXCJhdWRcIjpcInNoZW5hbmlnYW5cIixcInRpZFwiOlwiNzBiMDYwMjQtMjUxYS00ODE5LTk1MTYtODVkYjhmNmM4OTM0XCJ9Il0=
         const body = getRequestBody(request, variables, uploadables);
         const headers = {
             ...getHeaders(uploadables, token)
