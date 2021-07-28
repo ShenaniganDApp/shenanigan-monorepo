b59aaf79bb6bd5b35b01888abac70bd7dfca5c12
Tawnee <allprt89@gmail.com>
2021-07-28 09:44:30 -0600

    add progress bar icon toggling. remove on layout

--- packages/app/src/components/challenges/ProgressBar.tsx
+++ packages/app/src/components/challenges/ProgressBar.tsx
@@ -3,37 +3,17 @@ import { Text, View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { colors } from '../UI';
 
-type Props = {};
+type Props = {
+    progressLabels: string[];
+    index: number;
+};
 
 export const ProgressBar = ({ progressLabels, index }: Props): ReactElement => {
-    const [widthOffset, setWidthOffset] = useState(0);
-    const [rowWidth, setRowWidth] = useState(0);
-
     return (
         <View style={styles.container}>
-            <View
-                style={styles.row}
-                onLayout={(layout) =>
-                    setRowWidth(layout.nativeEvent.layout.width)
-                }
-            >
+            <View style={styles.row}>
                 {progressLabels.map((item, itemIndex) => (
-                    <View
-                        style={styles.itemContainer}
-                        key={item}
-                        onLayout={(layout) => {
-                            const viewWidth = layout.nativeEvent.layout.width;
-
-                            if (
-                                itemIndex === 0 ||
-                                itemIndex === progressLabels.length - 1
-                            ) {
-                                setWidthOffset(
-                                    (prevState) => prevState + viewWidth / 2
-                                );
-                            }
-                        }}
-                    >
+                    <View style={styles.itemContainer} key={item}>
                         <View style={styles.dotContainer}>
                             <View style={styles.dot} />
                             {index === itemIndex && (
@@ -53,24 +33,22 @@ export const ProgressBar = ({ progressLabels, index }: Props): ReactElement => {
                     </View>
                 ))}
             </View>
-            <View
-                style={[styles.divider, { width: rowWidth - widthOffset }]}
-            ></View>
+            <View style={styles.divider}></View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        marginBottom: '6%',
-        paddingHorizontal: '4%'
+        marginBottom: '6%'
     },
     row: {
         flexDirection: 'row',
         justifyContent: 'space-between'
     },
     itemContainer: {
-        alignItems: 'center'
+        alignItems: 'center',
+        flex: 1
     },
     dotContainer: {
         alignItems: 'center',
@@ -100,7 +78,7 @@ const styles = StyleSheet.create({
     },
     divider: {
         height: 2,
-        width: '96%',
+        width: '66.666%',
         backgroundColor: colors.altWhite,
         position: 'absolute',
         top: 11,
