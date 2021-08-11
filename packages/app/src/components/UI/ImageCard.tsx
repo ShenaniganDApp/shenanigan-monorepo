38c7ac142b02893ebed8dbbc9c643bb9bd25d6d3
Prasad <prasadwalvekar@gmail.com>
2021-08-11 14:14:45 +0530

    Code review changes

--- packages/app/src/components/UI/ImageCard.tsx
+++ packages/app/src/components/UI/ImageCard.tsx
@@ -99,6 +99,7 @@ const styles = StyleSheet.create({
     outcomeText: {
         fontWeight: '900',
         color: 'white',
-        fontSize: sizes.smallScreen ? 16 : 18
+        fontSize: sizes.smallScreen ? 16 : 18,
+        textAlign: 'center'
     }
 });
