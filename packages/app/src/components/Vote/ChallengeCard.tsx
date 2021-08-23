0f777eaae82050681b66b0e5d41424e413cac9dd
Tawnee <allprt89@gmail.com>
2021-08-23 09:55:51 -0600

    remove unused imports. pass down info to card

--- packages/app/src/components/Vote/ChallengeCard.tsx
+++ packages/app/src/components/Vote/ChallengeCard.tsx
@@ -2,20 +2,34 @@ import React, { ReactElement } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Button, Card, colors, ImageCard, sizes, Title } from '../UI';
 
-type Props = {};
+type Props = {
+    title: string;
+    username: string;
+    leadingOutcome: string;
+    percent: string;
+    votesToFlip: number;
+    hoursToVote: number;
+};
 
-export const ChallengeCard = (props: Props): ReactElement => {
+export const ChallengeCard = ({
+    title,
+    username,
+    leadingOutcome,
+    percent,
+    votesToFlip,
+    hoursToVote
+}: Props): ReactElement => {
     return (
         <View style={styles.container}>
             <Title size={24} style={styles.center}>
-                1 Handed Pushup
+                {title}
             </Title>
             <Text style={[styles.username, styles.textMd, styles.center]}>
-                YoungKidWarrior
+                {username}
             </Text>
             <View style={styles.row}>
                 <ImageCard
-                    height={sizes.smallScreen ? 160 : 200}
+                    height={205}
                     source={{
                         uri:
                             'https://images.unsplash.com/photo-1474224017046-182ece80b263?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80'
@@ -24,7 +38,7 @@ export const ChallengeCard = (props: Props): ReactElement => {
                 <View style={styles.cardContainer}>
                     <Card style={styles.card}>
                         <Title style={[styles.pink, styles.center]} size={45}>
-                            95
+                            {percent}
                             <Title size={30} style={styles.pink}>
                                 %
                             </Title>
@@ -32,18 +46,20 @@ export const ChallengeCard = (props: Props): ReactElement => {
                         <Text
                             style={[styles.pink, styles.textMd, styles.center]}
                         >
-                            I crushed it!
+                            {leadingOutcome}
                         </Text>
                         <Text
                             style={[styles.gray, styles.textMd, styles.center]}
                         >
-                            17 <Text style={styles.textSm}>votes to flip</Text>
+                            {votesToFlip}{' '}
+                            <Text style={styles.textSm}>votes to flip</Text>
                         </Text>
                         <Button title="VOTE" style={styles.button} />
                         <Text
                             style={[styles.gray, styles.textMd, styles.center]}
                         >
-                            5 <Text style={styles.textSm}>hours to vote</Text>
+                            {hoursToVote}{' '}
+                            <Text style={styles.textSm}>hours to vote</Text>
                         </Text>
                     </Card>
                 </View>
@@ -63,7 +79,6 @@ const styles = StyleSheet.create({
     },
     row: {
         flexDirection: 'row'
-        // justifyContent: 'space-between'
     },
     cardContainer: {
         marginLeft: '6%',
